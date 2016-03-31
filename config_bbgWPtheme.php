2b681240029b089eadef9c8c2d00a876c4b1cdf1
Brian Williamson <brianwilliamson@brianw.home>
2016-03-31 09:40:13 -0400

    Reconfiguring the the config for BBG. Also copied custom home php into a new CustomBBGHome.

--- config_bbgWPtheme.php
+++ config_bbgWPtheme.php
@@ -1,11 +1,11 @@
 <?php
 
 //this file is called in header.php.  These are used for various og/twitter/social media tags
-define("DEFAULT_DESCRIPTION", "Innovation Series is the Broadcasting Board of Governors Office of Design and Digital Media Strategy's ongoing discussion on innovation in media.");
+define("DEFAULT_DESCRIPTION", "The Broadcasting Board of Governors' mission is to inform, engage and connect people around the world in support of freedom and democracy.");
 define("DEFAULT_IMAGE", site_url() . "/wp-content/themes/bbginnovate/img/logo-agency-square.png");
 define("DEFAULT_TITLE",get_bloginfo('name'));
 define("DEFAULT_AUTHOR","");
 define("DEFAULT_KEYWORDS","");
-define("SITE_TITLE_MARKUP","Office of Digital <span class='bbg-header__site-title--breakline'>& Design Innovation</span>");
-//define("SITE_TITLE_MARKUP","Broadcasting <span class='bbg-header__site-title--breakline'>Board of Governors</span>");
+//define("SITE_TITLE_MARKUP","Office of Digital <span class='bbg-header__site-title--breakline'>& Design Innovation</span>");
+define("SITE_TITLE_MARKUP","Broadcasting <span class='bbg-header__site-title--breakline'>Board of Governors</span>");
 ?>
