7570f9a5dcb265b2b12e3dd94be01bce0dcb98dd
Joe Flowers <jflowers45@gmail.com>
2016-04-13 19:16:55 -0400

    fix template name

--- page-seniormanagement.php
+++ page-seniormanagement.php
@@ -5,7 +5,7 @@
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package bbgRedesign
-  template name: Board
+  template name: Senior Management
  */
 
 /***** BEGIN PROJECT PAGINATION LOGIC 
