212de3d2faab5f9a31732f6720fe7a44e628b930
Brian Williamson <brianwilliamson@brianw.home>
2016-04-21 16:34:22 -0400

    Updating the default page template to support featured images (and 3-column grid). Experimenting with serif for body text.

--- page.php
+++ page.php
@@ -16,22 +16,24 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="usa-grid">
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<div class="usa-grid-full">
 
-				<div class="bbg-post-footer">
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-				</div>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; // End of the loop. ?>
-			</div><!-- .usa-grid -->
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+					<div class="bbg-post-footer">
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+					</div>
+
+				<?php endwhile; // End of the loop. ?>
+			</div><!-- .usa-grid-full -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,40 +11,75 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("bbg__article"); ?>>
 
+
+	<?php
+		$hideFeaturedImage = get_post_meta( get_the_ID(), "hide_featured_image", true );
+		if ( has_post_thumbnail() && ( $hideFeaturedImage != 1 ) ) {
+			echo '<div class="usa-grid-full">';
+			$featuredImageClass = "";
+			$featuredImageCutline="";
+			$thumbnail_image = get_posts(array('p' => get_post_thumbnail_id(get_the_ID()), 'post_type' => 'attachment'));
+			if ($thumbnail_image && isset($thumbnail_image[0])) {
+				$featuredImageCutline=$thumbnail_image[0]->post_excerpt;
+			}
+			echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large">';
+			//echo '<div style="position: absolute;"><h5 class="bbg-label">Label</h5></div>';
+			echo the_post_thumbnail( 'large-thumb' );
+
+			echo '</div>';
+			echo '</div> <!-- usa-grid-full -->';
+
+			if ($featuredImageCutline != "") {
+				echo '<div class="usa-grid">';
+					echo "<div class='bbg__article-header__caption'>$featuredImageCutline</div>";
+				echo '</div> <!-- usa-grid -->';
+			}
+		}
+	?><!-- .bbg__article-header__thumbnail -->
+
+	<div class="usa-grid">
+
 	<?php echo bbginnovate_post_categories(); ?>
 	<!-- .bbg-label -->
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
 
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<ul class="bbg-post-share-menu">
-		<li class="bbg-post-share-menu-tool email"><a href="#"><span class="bbg-share-icon email"></span><span class="bbg-share-text ">Email</span></a></li>
-		<li class="bbg-post-share-menu-tool facebook"><a href="#"><span class="bbg-share-icon facebook"></span><span class="bbg-share-text ">Share</span></a></li>
-		<li class="bbg-post-share-menu-tool twitter"><a href="#"><span class="bbg-share-icon twitter"></span><span class="bbg-share-text ">Tweet</span></a></li>
-	</ul>
+					<div class="bbg__article-sidebar--left">
+						<h3 class="bbg__sidebar-label bbg__contact-label">Social media </h3>
+						<p>Social share links could go here and here and here and here.</p>
+						&nbsp;
+					</div><!-- .bbg__article-sidebar--left -->
 
-	<div class="entry-content bbg__article-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bbginnovate' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
 
-	<footer class="entry-footer bbg-post-footer 1234">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'bbginnovate' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+
+					<div class="entry-content bbg__article-content <?php echo $featuredImageClass; ?>">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+
+
+
+					<div class="bbg__article-sidebar">
+						<p>Sidebar info or widgets here?</p>
+					</div><!-- .bbg__article-sidebar -->
+
+
+
+
+
+		<footer class="entry-footer bbg-post-footer 1234">
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'bbginnovate' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	</div><!-- .usa-grid -->
 </article><!-- #post-## -->
