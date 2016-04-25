0e848f1d07fd935b9bdacda798edfcb06cd34f8c
Joe Flowers <jflowers45@gmail.com>
2016-04-25 15:33:45 -0400

    switch hover to click

--- js/navigation.js
+++ js/navigation.js
@@ -30,7 +30,7 @@
 		menu.className += ' nav-menu';
 	}
 
-	button.onclick = function() {
+	jQuery('#site-navigation').hover( function() {
 		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
 			container.className = container.className.replace( ' toggled', '' );
 			button.setAttribute( 'aria-expanded', 'false' );
@@ -40,7 +40,7 @@
 			button.setAttribute( 'aria-expanded', 'true' );
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
-	};
+	});
 
 	// Get all the link elements within the menu.
 	links    = menu.getElementsByTagName( 'a' );
@@ -67,7 +67,7 @@
 
 		/* clicking any top level nav item with children should show its children and hide all others */
 		// see http://stackoverflow.com/questions/7394796/jquery-click-event-how-to-tell-if-mouse-was-clicked-or-enter-key-was-pressed
-		jQuery("li.menu-item-has-children").on('click', function(e, enterKeyPressed) {
+		jQuery("li.menu-item-has-children").on('hover', function(e, enterKeyPressed) {
 			if (window.innerWidth >=600) {
 				if (jQuery(this).find("ul").hasClass('showChildren')) {
 					
@@ -90,14 +90,14 @@
 		});
 		jQuery("li.menu-item-has-children").keydown(function(e) {
 		  if(e.keyCode == 13) {
-		    jQuery(this).trigger("click", true);
+		    jQuery(this).mouseover();
 		    e.preventDefault();
 		  }
 		});
 
 		
 		/* clicking on the body should hide all subnav items */
-		jQuery(document).on('click', function(e){
+		jQuery(document).on('hover', function(e){
 			jQuery('.showChildren').toggleClass('showChildren');
 			jQuery("li.menu-item-has-children").addClass('hidden');
 		});
