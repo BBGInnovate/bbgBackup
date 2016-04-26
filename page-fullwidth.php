2ed7a00c0dafa4273359ae598ea7758c4c7346a3
Brian Williamson <brianwilliamson@brianw.home>
2016-04-26 15:39:15 -0400

    Adding a full-width template.

---
+++ page-fullwidth.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package bbgRedesign
+   template name: Full-width
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div class="usa-grid-full">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'fullwidth' ); ?>
+
+					<div class="bbg-post-footer">
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+					</div>
+
+				<?php endwhile; // End of the loop. ?>
+			</div><!-- .usa-grid-full -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php /*get_sidebar();*/ ?>
+<?php get_footer(); ?>
