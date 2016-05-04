a546e0d103db26028d8c672c956cdaecff818ff4
Brian Williamson <brianwilliamson@brianw.local>
2016-05-04 12:53:08 -0400

    Featured video.

--- functions.php
+++ functions.php
@@ -592,7 +592,7 @@ add_filter('embed_handler_html', 'custom_youtube_settings');
 add_filter('embed_oembed_html', 'custom_youtube_settings');
 
 function featured_video ($url) {
-	$url = str_replace($url, "watch", "embed");
+	$url = str_replace("watch", "embed", $url);
 	$return="<div class='bbg-embed-shell bbg__featured-video'><div class='embed-container'>";
 	$return.='<iframe src="' . $url . '" frameborder="0" allowfullscreen="" data-ratio="NaN" data-width="" data-height="" style="display: block; margin: 0px;"></iframe>';
 	$return.="</div></div>";
