60f409098e80dfa97121673ee82cd9fc1adde745
Joe Flowers <jflowers45@gmail.com>
2016-05-13 16:34:49 -0400

    debug

--- page-private-debug.php
+++ page-private-debug.php
@@ -28,9 +28,6 @@ get_header(); ?>
 						));
 						echo "<ul style='margin-left:20px'>";
 						foreach($pages as $page){
-
-							echo "<li><a target='_blank' href='$page->guid'>$page->post_title</a></li>";
-var_dump($page);
 							echo "<li><a target='_blank' href='" . get_permalink($page->ID) . "'>$page->post_title</a></li>";
 						}
 						echo "</ul>";
