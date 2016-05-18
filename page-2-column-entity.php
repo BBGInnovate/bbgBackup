ed86dbb268d0297bfb3563b68d33098b72875bb8
Brian Williamson <brianwilliamson@brianw.home>
2016-05-18 14:09:18 -0400

    killing comments on the 2-column entity page.

--- page-2-column-entity.php
+++ page-2-column-entity.php
@@ -512,12 +512,6 @@ get_header(); ?>
 
 
 					<div class="bbg-post-footer">
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
 					</div>
 
 				<?php endwhile; // End of the loop. ?>
