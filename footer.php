c4e77d4e2b5f503453903b27d56f768a4a3d2f15
Brian Williamson <brianwilliamson@brianw.home>
2016-05-29 21:39:56 -0400

    Experimenting with simplifying the footer.

--- footer.php
+++ footer.php
@@ -16,13 +16,14 @@
 
 	</div><!-- #content -->
 
-	<div class="usa-grid bbg-footer__return-to-top__container">
-		<div class="usa-footer-return-to-top bbg-footer__return-to-top">
+	<div class="usa-grid bbg__footer__return-to-top__container">
+		<div class="usa-footer-return-to-top bbg__footer__return-to-top">
 			<a href="#">Return to top</a>
 		</div>
 	</div>
 
-	<footer id="colophon" class="site-footer usa-footer usa-footer-big bbg-footer bbg-footer--big usa-sans" role="contentinfo">
+	<!--
+	<footer id="colophon" class="site-footer usa-footer usa-footer-big bbg__footer bbg__footer--big usa-sans" role="contentinfo">
 		<div class="usa-footer-primary-section">
 			<div class="usa-grid-full">
 				<nav class="usa-footer-nav usa-width-two-thirds">
@@ -54,7 +55,7 @@
 						<li><a href="http://www.bbg.gov/open">Open Gov</a></li>
 					</ul>
 				</nav>
-					<div class="usa-width-one-third usa-sign_up-block bbg-footer__search">
+					<div class="usa-width-one-third usa-sign_up-block bbg__footer__search">
 						<?php get_search_form ( $echo = true ) ?>
 					</div>
 			</div>
@@ -104,9 +105,71 @@
 						<a href="mailto:publicaffairs@bbg.gov">publicaffairs@bbg.gov</a>
 					</address>
 				</div>
-			</div><!-- .usa-grid -->
-		</div><!-- .usa-secondary-section -->
-	</footer><!-- #colophon -->
+			</div>
+		</div>
+	</footer>
+	-->
+	<footer class="usa-footer usa-footer-medium usa-sans" role="contentinfo">
+
+		<div class="usa-footer-secondary_section usa-footer-big-secondary-section">
+			<div class="usa-grid">
+				<div class="usa-footer-logo usa-width-one-half">
+					<a href="<?php echo site_url(); ?>">
+						<img class="usa-footer-logo-img" src="<?php echo get_template_directory_uri() ?>/img/logo-agency-square.png" alt="Broadcasting Board of Governors logo">
+						<h3 class="usa-footer-logo-heading">Broadcasting Board of Governors</h3>
+					</a>
+				</div>
+
+				<div class="usa-footer-contact-links usa-width-one-half">
+					<div class="usa-social-links">
+						<a href="https://www.facebook.com/BBGgov/">
+							<svg width="26" height="39" role="img" aria-label="Facebook">
+								<title>Facebook</title>
+								<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_template_directory_uri() ?>/img/social-icons/svg/facebook25.svg" src="<?php echo get_template_directory_uri() ?>/img/social-icons/png/facebook25.png" width="26" height="39"></image>
+							</svg>
+						</a>
+						<a href="https://twitter.com/BBGgov">
+							<svg width="26" height="39" role="img" aria-label="Twitter">
+							<title>Twitter</title>
+							<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_template_directory_uri() ?>/img/social-icons/svg/twitter16.svg" src="<?php echo get_template_directory_uri() ?>/img/social-icons/png/twitter16.png" width="26" height="39"></image>
+							</svg>
+						</a>
+						<a href="https://www.youtube.com/user/bbgtunein">
+							<svg width="26" height="39" role="img" aria-label="YouTube">
+								<title>YouTube</title>
+								<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_template_directory_uri() ?>/img/social-icons/svg/youtube15.svg" src="<?php echo get_template_directory_uri() ?>/img/social-icons/png/youtube15.png" width="26" height="39"></image>
+							</svg>
+						</a>
+						<a href="http://www.bbg.gov/category/press-release/feed/">
+							<svg width="26" height="39" role="img" aria-label="RSS">
+								<title>RSS</title>
+								<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo get_template_directory_uri() ?>/img/social-icons/svg/rss25.svg" src="<?php echo get_template_directory_uri() ?>/img/social-icons/png/rss25.png" width="26" height="39"></image>
+							</svg>
+						</a>
+					</div>
+
+					<address>
+						<h3 class="usa-footer-contact-heading">Contact the BBG</h3>
+						<p>(202) 203-4400</p>
+						<a href="mailto:publicaffairs@bbg.gov">publicaffairs@bbg.gov</a>
+					</address>
+				</div>
+			</div>
+		</div>
+		<div class="bbg__footer__sub">
+			<div class="usa-grid">
+				<div>
+					<div class="bbg__footer__sub__required-link"><a href="/about-the-agency/research-reports/foia/">FOIA</a> |</div>
+					<div class="bbg__footer__sub__required-link"><a href="http://oig.state.gov/hotline/" onclick="__gaTracker('send', 'event', 'outbound-widget', 'http://oig.state.gov/hotline/', 'Inspector General Hotline');">Inspector General Hotline</a></div> | 
+					<div class="bbg__footer__sub__required-link"><a href="/sitemap">Sitemap</a> |</div>
+					<div class="bbg__footer__sub__required-link"><a href="privacy-policy">Privacy Policy</a> |</div>
+					<div class="bbg__footer__sub__required-link"><a href="http://www.usa.gov/" onclick="__gaTracker('send', 'event', 'outbound-widget', 'http://www.usa.gov/', 'USA.gov');">USA.gov</a> |</div>
+					<div class="bbg__footer__sub__required-link"><a href="/open">Open Gov</a></div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
