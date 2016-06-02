548e7654d939de3447f00959eef20c8eaf5453d3
Joe Flowers <jflowers45@gmail.com>
2016-06-01 21:58:25 -0400

    Rename deprecated entity layout, fix reach label on the real one.

--- page-2-column-entity.php
+++ page-2-column-entity.php
@@ -74,7 +74,7 @@ if ($languages != "") {
 	}
 }
 if ($audience != "") {
-	$audience = '<li><span class="bbg__article-sidebar__list-label">Audience reach: </span>'. $audience . '</li>';
+	$audience = '<li><span class="bbg__article-sidebar__list-label">Audience estimate: </span>'. $audience . '</li>';
 }
 if ($appLink != "") {
 	$appLink = '<h3><a href="https://innovation.bbg.gov/mobileapps/" class="bbg__article-sidebar__list-label">Download the apps </a></h3><p style="font-family: sans-serif; margin-top: 0;">'. $appLink . '</p>';
