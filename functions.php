17091121620080fdf3a4df00d4fada7f1785e85f
Joe Flowers <jflowers45@gmail.com>
2016-06-01 22:49:47 -0400

    quotations in their own file

--- functions.php
+++ functions.php
@@ -261,6 +261,12 @@ require get_template_directory() . '/inc/bbg-functions-tinyMCE.php';
 require get_template_directory() . '/inc/bbg-functions-shortcodes.php';
 
 /**
+ * Load BBG Quotations
+ */
+require get_template_directory() . '/inc/bbg-functions-quotations.php';
+
+
+/**
  * Add Twitter handle to author metadata using built-in wp hook for contact methods
  * Reference: http://www.paulund.co.uk/how-to-display-author-bio-with-wordpress
  */
@@ -371,12 +377,6 @@ function featured_video ($url) {
 	return $return;
 }
 
-	/*
-	add_filter( 'pre_update_option_blogname', 'blogname_with_html', 10, 2 );
-	function blogname_with_html( $value, $old_value ) {
-		return "saved something";
-	}
-	*/
 
 /*===================================================================================
  * CUSTOM POST CATEGORY LIST LOGIC
@@ -1319,116 +1319,7 @@ function toolbar_link_to_mypage( $wp_admin_bar ) {
 	$wp_admin_bar->add_node( $args );
 }
 
-function getAllQuotes($entity, $idsToExclude) {
-	//	allEntities or rfa, rferl, voa, mbn, ocb
-	if ($entity=='allEntities') {
-		$qParams = array(
-			'post_type'=> 'post',
-			'post_status' => 'publish',
-			'order' => 'DESC',
-			'tax_query' => array(
-				array(
-					'taxonomy' => 'post_format',
-					'field' => 'slug',
-					'terms' => array( 'post-format-quote' )
-				)
-			),
-			'post__not_in' => $idsToExclude
-		);
-	} else {
-		$qParams = array(
-			'post_type'=> 'post',
-			'post_status' => 'publish',
-			'order' => 'DESC',
-			'tax_query' => array(
-				'relation' => 'AND',
-                array(
-			        'taxonomy' => 'post_format',
-			        'field'    => 'slug',
-			        'terms'    => array( 'post-format-quote' ),
-                ),
-				array(
-					'taxonomy' => 'category',
-					'field' => 'slug',
-					'terms' => array( $entity )
-				),
-			),
-			'post__not_in' => $idsToExclude
-		);
-	}
 
-	$quotes=array();
-	$custom_query = new WP_Query($qParams);
-	while ( $custom_query -> have_posts() )  {
-		$custom_query->the_post();
-		$id = get_the_ID();
-		$speaker = get_post_meta( $id, 'quotation_speaker', true );
-		$quoteTagline = get_post_meta( $id, 'quotation_tagline', true );
-		//$quoteDate = get_post_meta( $id, 'quotation_date', true );
-		$quoteDate = get_field( 'quotation_date', $id, true );
-
-
-		$quoteMugshotID=get_post_meta( get_the_ID(), 'quotation_mugshot', true );
-		$quoteMugshot = "";
-
-		if ($quoteMugshotID) {
-			$quoteMugshot = wp_get_attachment_image_src( $quoteMugshotID , 'mugshot');
-			$quoteMugshot = $quoteMugshot[0];
-		}
-
-
-		$quotes[] = array(
-			'ID' => $id,
-			'url' => get_permalink($id),
-			'quoteDate' => $quoteDate,
-			'speaker' => $speaker,
-			'quoteText' => get_the_content(),
-			'quoteTagline' => $quoteTagline,
-			'quoteMugshot' => $quoteMugshot
-		);
-	}
-	wp_reset_postdata();
-	return $quotes;
-}
-function getRandomQuote($entity, $idsToExclude) {
-	//	allEntities or rfa, rferl, voa, mbn, ocb
-	$allQuotes = getAllQuotes($entity, $idsToExclude);
-	$returnVal = false;
-	if (count($allQuotes)) {
-		$randKey = array_rand($allQuotes);
-		$returnVal = $allQuotes[$randKey];
-	}
-	return $returnVal;
-}
-
-function outputQuote($q, $class="") {
-	$quoteDate = $q['quoteDate'];
-	$ID = $q['ID'];
-	$url = $q['url'];
-	$speaker = $q['speaker'];
-	$quoteText = $q['quoteText'];
-	$tagline = $q['quoteTagline'];
-	if ($tagline != "") {
-		$tagline = "" . $tagline;
-	}
-	$mugshot = $q['quoteMugshot'];
-
-	$quote = "";
-	$quote .= "<div class='bbg__quotation $class'>";
-	$quote .= "<h2 class='bbg__quotation-text--large'>&ldquo;$quoteText&rdquo;</h2>";
-	$quote .= "<div class='bbg__quotation-attribution__container'>";
-	$quote .= "<p class='bbg__quotation-attribution'>";
-
-	if ($mugshot != "") {
-		$quote .= "<img src='$mugshot' class='bbg__quotation-attribution__mugshot'/>";
-	}
-	$quote .= "<span class='bbg__quotation-attribution__text'>";
-	$quote .= "<span class='bbg__quotation-attribution__name'>$speaker</span>";
-	$quote .= "<span class='bbg__quotation-attribution__credit'>$tagline</span>";
-	$quote .= "</span></p></div>";
-	$quote .= "</div>";
-	echo $quote;
-}
 
 if ( function_exists ('acf_add_options_page') ) {
 	acf_add_options_page (array(
---
+++ inc/bbg-functions-quotations.php
@@ -0,0 +1,113 @@
+<?php 
+	function getAllQuotes($entity, $idsToExclude) {
+		//	allEntities or rfa, rferl, voa, mbn, ocb
+		if ($entity=='allEntities') {
+			$qParams = array(
+				'post_type'=> 'post',
+				'post_status' => 'publish',
+				'order' => 'DESC',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'post_format',
+						'field' => 'slug',
+						'terms' => array( 'post-format-quote' )
+					)
+				),
+				'post__not_in' => $idsToExclude
+			);
+		} else {
+			$qParams = array(
+				'post_type'=> 'post',
+				'post_status' => 'publish',
+				'order' => 'DESC',
+				'tax_query' => array(
+					'relation' => 'AND',
+	                array(
+				        'taxonomy' => 'post_format',
+				        'field'    => 'slug',
+				        'terms'    => array( 'post-format-quote' ),
+	                ),
+					array(
+						'taxonomy' => 'category',
+						'field' => 'slug',
+						'terms' => array( $entity )
+					),
+				),
+				'post__not_in' => $idsToExclude
+			);
+		}
+
+		$quotes=array();
+		$custom_query = new WP_Query($qParams);
+		while ( $custom_query -> have_posts() )  {
+			$custom_query->the_post();
+			$id = get_the_ID();
+			$speaker = get_post_meta( $id, 'quotation_speaker', true );
+			$quoteTagline = get_post_meta( $id, 'quotation_tagline', true );
+			//$quoteDate = get_post_meta( $id, 'quotation_date', true );
+			$quoteDate = get_field( 'quotation_date', $id, true );
+
+
+			$quoteMugshotID=get_post_meta( get_the_ID(), 'quotation_mugshot', true );
+			$quoteMugshot = "";
+
+			if ($quoteMugshotID) {
+				$quoteMugshot = wp_get_attachment_image_src( $quoteMugshotID , 'mugshot');
+				$quoteMugshot = $quoteMugshot[0];
+			}
+
+
+			$quotes[] = array(
+				'ID' => $id,
+				'url' => get_permalink($id),
+				'quoteDate' => $quoteDate,
+				'speaker' => $speaker,
+				'quoteText' => get_the_content(),
+				'quoteTagline' => $quoteTagline,
+				'quoteMugshot' => $quoteMugshot
+			);
+		}
+		wp_reset_postdata();
+		return $quotes;
+	}
+	function getRandomQuote($entity, $idsToExclude) {
+		//	allEntities or rfa, rferl, voa, mbn, ocb
+		$allQuotes = getAllQuotes($entity, $idsToExclude);
+		$returnVal = false;
+		if (count($allQuotes)) {
+			$randKey = array_rand($allQuotes);
+			$returnVal = $allQuotes[$randKey];
+		}
+		return $returnVal;
+	}
+
+	function outputQuote($q, $class="") {
+		$quoteDate = $q['quoteDate'];
+		$ID = $q['ID'];
+		$url = $q['url'];
+		$speaker = $q['speaker'];
+		$quoteText = $q['quoteText'];
+		$tagline = $q['quoteTagline'];
+		if ($tagline != "") {
+			$tagline = "" . $tagline;
+		}
+		$mugshot = $q['quoteMugshot'];
+
+		$quote = "";
+		$quote .= "<div class='bbg__quotation $class'>";
+		$quote .= "<h2 class='bbg__quotation-text--large'>&ldquo;$quoteText&rdquo;</h2>";
+		$quote .= "<div class='bbg__quotation-attribution__container'>";
+		$quote .= "<p class='bbg__quotation-attribution'>";
+
+		if ($mugshot != "") {
+			$quote .= "<img src='$mugshot' class='bbg__quotation-attribution__mugshot'/>";
+		}
+		$quote .= "<span class='bbg__quotation-attribution__text'>";
+		$quote .= "<span class='bbg__quotation-attribution__name'>$speaker</span>";
+		$quote .= "<span class='bbg__quotation-attribution__credit'>$tagline</span>";
+		$quote .= "</span></p></div>";
+		$quote .= "</div>";
+		echo $quote;
+	}
+
+?>
