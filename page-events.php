13f94fa736c2c3a1e511781b9fa9cb5760334e2d
Joe Flowers <jflowers45@gmail.com>
2016-06-02 20:03:54 -0400

    minor events cleanup

--- page-events.php
+++ page-events.php
@@ -26,8 +26,8 @@ if ($currentPage > 1) {
 }
 
 $hasTeamFilter=false;
-$mobileAppsPostContent="";
 
+/**** QUERY PAST EVENTS FOR MAIN PAGE LOOP ***/
 $qParams=array(
 	'post_type' => array('post')
 	,'cat' => get_cat_id('Event')
@@ -35,8 +35,6 @@ $qParams=array(
 	,'offset' => $offset
 	,'post_status' => array('publish')
 );
-
-/*** late in the game we ran into a pagination issue, so we're running a second query here ***/
 $past_events_query_args= $qParams;
 $past_events_query = new WP_Query( $past_events_query_args );
 
@@ -44,17 +42,28 @@ $totalPages=1;
 if ($past_events_query->found_posts > $numPostsFirstPage) {
 	$totalPages = 1 + ceil( ($past_events_query->found_posts - $numPostsFirstPage)/$numPostsSubsequentPages);
 }
+/**** END QUERY PAST EVENTS FOR MAIN PAGE LOOP ***/
 
-$portfolioDescription="some portfolio description could go here based on the category description?";
+/**** QUERY FUTURE EVENTS  ***/
+$qParamsUpcoming = array(
+	'post_type' => array('post')
+	,'cat' => get_cat_id('Event')
+	,'posts_per_page' => $postsPerPage
+	,'offset' => $offset
+	,'post_status' => array('future')
+	,'order' => 'ASC'
+);
+$future_events_query_args = $qParamsUpcoming;
+$future_events_query = new WP_Query( $future_events_query_args );
+/**** END QUERY FUTURE EVENTS  ***/
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			
+			<!-- BEGIN HEADER RED LABEL -->
 			<div class="usa-grid">
-
-			<?php if ( $past_events_query->have_posts() ) : ?>
-
 				<header class="page-header">
 					<h6 class="bbg-label--mobile large">
 						<?php if ($hasTeamFilter) {
@@ -62,81 +71,72 @@ get_header(); ?>
 						} else {
 							echo 'Events';
 						}
-
 						?>
 					</h6>
 				</header><!-- .page-header -->
 			</div>
-			
+			<!-- END HEADER -->
+
+			<!-- BEGIN PAST EVENTS -->
 			<div class="usa-grid-full">
+			<?php 
+				$counter = 0;
+				while ( $past_events_query->have_posts() ) {
+					$past_events_query->the_post(); 
+					$counter++;
+					if (  (!is_paged() && $counter==1) ){
+						get_template_part( 'template-parts/content-excerpt-featured', get_post_format() );
+					} else {
+						if( (!is_paged() && $counter == 2) || (is_paged() && $counter==1) ){
+							echo '</div>';
+							echo '<div class="usa-grid">';
+							echo '<div class="bbg-grid--1-1-1-2 secondary-stories">';
+						} elseif( (!is_paged() && $counter == 4) || (is_paged() && $counter==3)){
+							echo '</div><!-- left column -->';
+							echo '<div class="bbg-grid--1-1-1-2 tertiary-stories">';
+							echo '<header class="page-header">';
+							echo '<h6 class="page-title bbg-label small">More events</h6>';
+							echo '</header>';
 
-				<?php /* Start the Loop */ 
-					$counter = 0;
-				?>
-
-
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( $past_events_query->have_posts() ) : $past_events_query->the_post(); ?>
-
-					<?php
-						$counter++;
-													//Add a check here to only show featured if it's not paginated.
-						if (  (!is_paged() && $counter==1) ){
-							get_template_part( 'template-parts/content-excerpt-featured', get_post_format() );
-						} else {
-							if( (!is_paged() && $counter == 2) || (is_paged() && $counter==1) ){
-								echo '</div>';
-								echo '<div class="usa-grid">';
-								echo '<div class="bbg-grid--1-1-1-2 secondary-stories">';
-							} elseif( (!is_paged() && $counter == 4) || (is_paged() && $counter==3)){
-								echo '</div><!-- left column -->';
-								echo '<div class="bbg-grid--1-1-1-2 tertiary-stories">';
-								echo '<header class="page-header">';
-								echo '<h6 class="page-title bbg-label small">More events</h6>';
-								echo '</header>';
-
-								//These values are used for every excerpt >=4
-								$includeImage = FALSE;
-								$includeMeta = FALSE;
-								$includeExcerpt=FALSE;
-							}
-							get_template_part( 'template-parts/content-excerpt-list', get_post_format() );
+							//These values are used for every excerpt >=4
+							$includeImage = FALSE;
+							$includeMeta = FALSE;
+							$includeExcerpt=FALSE;
 						}
-					?>
-
-				<?php endwhile; ?>
-
-				</div><!-- .usa-grid -->
-				
-				<?php 
-					echo '<nav class="navigation posts-navigation" role="navigation">';
-					echo '<h2 class="screen-reader-text">Event navigation</h2>';
-					echo '<div class="nav-links">';
-					$nextLink=get_next_posts_link('Older Events', $totalPages);
-					$prevLink=get_previous_posts_link('Newer Events');
-					if ($nextLink != "") {
-						echo '<div class="nav-previous">';
-						echo $nextLink;
-						echo '</div>';
+						get_template_part( 'template-parts/content-excerpt-list', get_post_format() );
 					}
-
-					if ($prevLink != "") {
-						echo '<div class="nav-next">';
-						echo $prevLink;
-						echo '</div>';	
-					}
-					
-					echo '</div>';
-					echo '</nav>';
-
-					
-				?>
-
-
-
-			<?php endif; ?>
+				}
+			?>
 			</div><!-- .usa-grid-full -->
+			<!-- END PAST EVENTS -->
+
+			<!-- BEGIN NAVIGATION  -->
+			<div class="usa-grid-full">
+			<?php 
+				echo '<nav class="navigation posts-navigation" role="navigation">';
+				echo '<h2 class="screen-reader-text">Event navigation</h2>';
+				echo '<div class="nav-links">';
+				$nextLink=get_next_posts_link('Older Events', $totalPages);
+				$prevLink=get_previous_posts_link('Newer Events');
+				if ($nextLink != "") {
+					echo '<div class="nav-previous">';
+					echo $nextLink;
+					echo '</div>';
+				}
+
+				if ($prevLink != "") {
+					echo '<div class="nav-next">';
+					echo $prevLink;
+					echo '</div>';	
+				}
+				
+				echo '</div>';
+				echo '</nav>';
+			?>
+			</div>
+			<!-- END NAVIGATION  -->
+			
+			<!-- BEGIN FUTURE EVENTS  -->
 			<div class="usa-grid-full">
 			<?php
 				if (!is_paged()) {
@@ -144,28 +144,10 @@ get_header(); ?>
 					echo '<header class="page-header">';
 					echo '<h6 class="page-title bbg-label small">Upcoming events</h6>';
 					echo '</header>';
-
-					$qParamsUpcoming = array(
-						'post_type' => array('post')
-						,'cat' => get_cat_id('Event')
-						,'posts_per_page' => $postsPerPage
-						,'offset' => $offset
-						,'post_status' => array('future')
-						,'order' => 'ASC'
-					);
-					$future_events_query_args = $qParamsUpcoming;
-					$future_events_query = new WP_Query( $future_events_query_args );
 					while ( $future_events_query->have_posts() ) {
 						$future_events_query->the_post(); 
 						$counter++;
-						//These values are used for every excerpt >=4
-						$includeImage = FALSE;
-						$includeMeta = FALSE;
-						$includeExcerpt = FALSE;
-						//get_template_part( 'template-parts/content-excerpt-list', get_post_format() );
-
-						//I'm not using get_template_part because of how future permalinks, but we could make that work if we needed
-
+						//we're not using get_template_part because of how future permalinks work
 						echo '<article id="post-' .get_the_ID() . '" ' . get_post_class($classNames) . '>';
 						global $post;
 						$my_post = clone $post;
@@ -174,15 +156,12 @@ get_header(); ?>
 						$permalink = get_permalink($my_post);
 						echo "<a href='$permalink'>" . get_the_title() . "</a>";
 						echo '</article>';
-
-
-
-
 					}
 					echo '</section>';
 				}
 			?>
 			</div>
+			<!-- END FUTURE EVENTS  -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
