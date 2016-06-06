b52c4e726cf885f2af9908f4e3ae01b33a5e5725
Brian Williamson <brianwilliamson@brianw.local>
2016-06-06 13:20:12 -0400

    Fixing duplicate header label.

--- page-2-column.php
+++ page-2-column.php
@@ -49,16 +49,6 @@ get_header(); ?>
 									//borrowed from: https://wordpress.org/support/topic/link-to-parent-page
 									$parent = $wpdb->get_row("SELECT post_title FROM $wpdb->posts WHERE ID = $post->post_parent");
 									$parent_link = get_permalink($post->post_parent);
-								?>
-								<h5 class="bbg-label--mobile large"><a href="<?php echo $parent_link; ?>"><?php echo $parent->post_title; ?></a></h5>
-	
-								<?php } ?>
-
-
-								<?php if($post->post_parent) {
-									//borrowed from: https://wordpress.org/support/topic/link-to-parent-page
-									$parent = $wpdb->get_row("SELECT post_title FROM $wpdb->posts WHERE ID = $post->post_parent");
-									$parent_link = get_permalink($post->post_parent);
 									?>
 									<h5 class="bbg-label--mobile large"><a href="<?php echo $parent_link; ?>"><?php echo $parent->post_title; ?></a></h5>
 								<?php } else{ ?>
