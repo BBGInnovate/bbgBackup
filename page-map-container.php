b0a27e5c1935f5472c7c406f862fe1e5cc1a3d42
Brian Williamson <brianwilliamson@brianw.local>
2016-06-15 10:50:37 -0400

    Minor map container style changes.

--- page-map-container.php
+++ page-map-container.php
@@ -34,11 +34,28 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="usa-grid-full">
 
-				<?php echo $pageContent; ?>
+				<div class="usa-grid">
+					<header class="page-header">
+
+						<?php if($post->post_parent) {
+							//borrowed from: https://wordpress.org/support/topic/link-to-parent-page
+							$parent = $wpdb->get_row("SELECT post_title FROM $wpdb->posts WHERE ID = $post->post_parent");
+							$parent_link = get_permalink($post->post_parent);
+						?>
+						<h5 class="bbg-label--mobile large"><a href="<?php echo $parent_link; ?>"><?php echo $parent->post_title; ?></a></h5>
+
+						<?php } ?>
+
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+					</header><!-- .page-header -->
+
+					<?php echo $pageContent; ?>
+				</div>
 
 				<img id="loading" src="img/loading.gif" />
 				<div class="usa-grid">
-					<select id="entity">
+					<select id="entity" style="margin-bottom: 3rem;">
 						<option value="voa">VOA</option>
 						<option value="rfa">RFA</option>
 						<option value="rferl">RFERL</option>
