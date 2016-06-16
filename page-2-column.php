5fbd7a8482aab6e8141deae6074e18c5fa22ba3d
Gigi Frias <gigi.frias@gmail.com>
2016-06-16 18:06:29 -0400

    Making some of the download options dynamic

--- page-2-column.php
+++ page-2-column.php
@@ -34,11 +34,11 @@ $sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
 $sidebarDownloads = "";
 if( $sidebarInclude ){
 	$rows = get_field('sidebar_downloads');
-	if($rows) {
+	if ( $rows ) {
 		$s = '<form style="">';
-		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">Download a previous year</label>';
+		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
 		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-		$s .= '<option>Select a file to download</option>';
+		$s .= '<option>' . $optionDefault . '</option>';
 
 		foreach( $rows as $row ) {
 			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
@@ -62,7 +62,7 @@ get_header(); ?>
 
 			<div class="usa-grid-full">
 
-				<?php while ( have_posts() ) : the_post(); 
+				<?php while ( have_posts() ) : the_post();
 					//$videoUrl = get_post_meta( get_the_ID(), 'featured_video_url', true );
 				?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class("bbg__article"); ?>>
@@ -146,10 +146,10 @@ get_header(); ?>
 
 
 
-								<?php 
+								<?php
 									//Add blog posts below the main content
 									$relatedCategory=get_field('related_category_posts', $id);
-									
+
 									if ( $relatedCategory != "" ) {
 										$qParams2=array(
 											'post_type' => array('post'),
@@ -179,7 +179,7 @@ get_header(); ?>
 
 							<div class="bbg__article-sidebar large">
 
-								<?php 
+								<?php
 									if ( $secondaryColumnContent != "" ) {
 										echo $secondaryColumnContent;
 									}
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -135,12 +135,13 @@ $sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
 $sidebarDownloads = "";
 if( $sidebarInclude ) {
 	$downloadsTitle = get_field( 'sidebar_downloads_title' );
+	$optionDefault = get_field ( 'sidebar_downloads_default' );
 	$rows = get_field( 'sidebar_downloads' );
-	if( $rows ) {
+	if ( $rows ) {
 		$s = '<form style="">';
 		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
 		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-		$s .= '<option>Select a download</option>';
+		$s .= '<option>' . $optionDefault . '</option>';
 
 		foreach( $rows as $row ) {
 			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
