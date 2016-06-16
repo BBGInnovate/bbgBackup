e57cabb0f8be5fbbb17bc4aeed4d64b805ee4241
Gigi Frias <gigi.frias@gmail.com>
2016-06-16 17:44:08 -0400

    Updating form title and option fields

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -133,13 +133,14 @@ if ( $includeSidebar ) {
 // Sidebar multiple downloads drop-down
 $sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
 $sidebarDownloads = "";
-if( $sidebarInclude ){
+if( $sidebarInclude ) {
+	$downloadsTitle = get_field( 'sidebar_downloads_title' );
 	$rows = get_field( 'sidebar_downloads' );
 	if( $rows ) {
 		$s = '<form style="">';
-		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">Download a previous year</label>';
+		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">$downloadTitles</label>';
 		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-		$s .= '<option>Select a file to download</option>';
+		$s .= '<option>Select a download</option>';
 
 		foreach( $rows as $row ) {
 			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
