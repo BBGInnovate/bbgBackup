e3c1062c227e9fbed343d32501b0ff3160391444
Joe Flowers <jflowers45@gmail.com>
2016-06-24 10:25:25 -0400

    formatBytes in decimal rather than binary.  Most filesystems show 1024 base

--- functions.php
+++ functions.php
@@ -91,7 +91,7 @@ function formatBytes($bytes, $precision = 2) {
 	$units = array('B', 'KB', 'MB', 'GB', 'TB'); 
 
 	$bytes = max($bytes, 0); 
-	$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+	$pow = floor(($bytes ? log($bytes) : 0) / log(1000)); 
 	$pow = min($pow, count($units) - 1); 
 
 	// Uncomment one of the following alternatives
