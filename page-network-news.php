2c4e32e6be0020dea789477778348c8d5e94d6d1
Joe Flowers <jflowers45@gmail.com>
2016-06-24 17:10:35 -0400

    removed sample geoJSON into a scratchpad file ... if we want to keep it we'll put it somewhere else

--- page-network-news.php
+++ page-network-news.php
@@ -146,51 +146,6 @@ $qParams=array(
 	,'post_status' => array('publish')
 );
 
-/*
-//Sample GeoJSON format
-var geojson = [
-	{
-		"type": "FeatureCollection",
-		"features": [
-		{
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-77.016556,
-					38.887226
-				]
-			},
-			"properties": {
-				"title": "Africa Rizing HQ",
-				"description": "description could go here.",
-				"marker-color": "#F7941E",
-				"marker-size": "large",
-				"marker-symbol": "building"
-			}
-		},
-
-		{
-			"type": "Feature",
-			"geometry": {
-			"type": "Point",
-			"coordinates": [
-			  -0.200000,
-			  5.550000
-			]
-			},
-			"properties": {
-			"title": "Adam Martin (<a href='http://twitter.com/'>@adamjmartin</a>) — Accra, Ghana",
-			"description": "<img src='http://54.243.239.169/brian/africa.rizing/images/mugshot_adamjmartin.jpg' style='width: 30%; float: left; margin-right: 10px; '> #BOS #DCA #ACC Tweets on #beisbol #media #tech dir. of tech & innovation @BBGInnovate former #pubmedia @NPRTechTeam and @NPRNews always RadioBoston dot Com",
-			"marker-color": "#FBB040",
-			"marker-size": "large"
-			}
-		}
-	  ]
-	}
-];
-*/
-
 /*** late in the game we ran into a pagination issue, so we're running a second query here ***/
 $custom_query_args= $qParams;
 $custom_query = new WP_Query( $custom_query_args );
---
+++ scratchpad.php
@@ -0,0 +1,45 @@
+
+/*
+//Sample GeoJSON format
+var geojson = [
+	{
+		"type": "FeatureCollection",
+		"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-77.016556,
+					38.887226
+				]
+			},
+			"properties": {
+				"title": "Africa Rizing HQ",
+				"description": "description could go here.",
+				"marker-color": "#F7941E",
+				"marker-size": "large",
+				"marker-symbol": "building"
+			}
+		},
+
+		{
+			"type": "Feature",
+			"geometry": {
+			"type": "Point",
+			"coordinates": [
+			  -0.200000,
+			  5.550000
+			]
+			},
+			"properties": {
+			"title": "Adam Martin (<a href='http://twitter.com/'>@adamjmartin</a>) — Accra, Ghana",
+			"description": "<img src='http://54.243.239.169/brian/africa.rizing/images/mugshot_adamjmartin.jpg' style='width: 30%; float: left; margin-right: 10px; '> #BOS #DCA #ACC Tweets on #beisbol #media #tech dir. of tech & innovation @BBGInnovate former #pubmedia @NPRTechTeam and @NPRNews always RadioBoston dot Com",
+			"marker-color": "#FBB040",
+			"marker-size": "large"
+			}
+		}
+	  ]
+	}
+];
+*/
