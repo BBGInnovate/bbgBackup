1fd53f2f90575fede2ba0ffb7aad0cb6c20a2aff
Brian Williamson <brianwilliamson@brianw.local>
2016-06-24 13:52:47 -0400

    Display banners on profile posts.

--- single-profile.php
+++ single-profile.php
@@ -114,7 +114,7 @@ get_header(); ?>
 
 						$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 700,450 ), false, '' );
 
-						echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__profile-header__banner" style="background-image: url('.$src[0].'); background-position: '.$bannerPosition.'">';
+						echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__article-header__banner--profile" style="background-image: url('.$src[0].'); background-position: '.$bannerPosition.'">';
 						echo '</div>';
 						echo '</div> <!-- usa-grid-full -->';
 					}
