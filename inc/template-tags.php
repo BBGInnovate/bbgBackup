ea42ad9249aa7a7648a18d02788aff5be3385574
Joe Flowers <jflowers45@gmail.com>
2016-06-29 17:33:14 -0400

    initial stab at byline override

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -28,12 +28,26 @@ function bbginnovate_posted_on() {
 		//'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'bbginnovate' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
 
-	echo '<span class="byline"> ' . $byline . '</span> <span class="u--seperator"> </span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+	$byline = "";
+	$includeByline = get_post_meta( get_the_ID(), 'include_byline', true );
+	if ( $includeByline ){
+		$bylineOverride = get_post_meta( get_the_ID(), 'byline_override', true );
+		if ($bylineOverride == "") {
+			$byline = sprintf(
+				esc_html_x( 'by %s', 'post author', 'bbginnovate' ),
+				'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			);
+		} else {
+			$byline = '<span class="bbg__article-byline">by ' . $bylineOverride . "</span>";
+		}
+		$byline = '<span class="byline"> ' . $byline . '</span> <span class="u--seperator"> </span><span class="posted-on">' . $posted_on . '</span>'; 
+	} else {
+		$byline = '<span class="posted-on">' . $posted_on . '</span>'; 
+	}
+	echo $byline;
+
 
 }
 endif;
