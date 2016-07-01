531229f0641ce765b61194b8720da73faa5a6dc0
Brian Williamson <brianwilliamson@brianw.home>
2016-07-01 09:15:33 -0400

    Event page dates.

--- template-parts/content-excerpt-list.php
+++ template-parts/content-excerpt-list.php
@@ -67,7 +67,7 @@ $linkImage = sprintf( '<a href="%s" rel="bookmark" tabindex="-1">', esc_url( get
 			echo '<h5 class="bbg__excerpt__event-date">' . get_the_date() . '</h5>';
 		} ?>
 
-		<?php the_title( sprintf( '<h3 class="entry-title bbg-blog__excerpt-title--list"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title bbg-blog__excerpt-title--list <?php if ($includeDate) { echo "bbg__excerpt-title--showDate"; };?>"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 
 	</header><!-- .bbg-blog__excerpt-header -->
