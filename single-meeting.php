215be265f7c8cb9ee5a10f487b4a27f36fb8b664
Gigi Frias <gigi.frias@gmail.com>
2016-07-06 17:05:27 -0400

    tweak

--- single-meeting.php
+++ single-meeting.php
@@ -390,8 +390,9 @@ get_header(); ?>
 
 													// $relatedProfile = '<div class="bbg__sidebar__primary">';
 													// $relatedProfile = '<a href="' . $profileLink . '"><img class="bbg__sidebar__primary-image" src="'. $profilePhoto .'"/></a>';
-													$relatedProfile = '<h3 class="bbg__sidebar__primary-headline"><a href="' . $profileLink . '">' . $profileName . '</a></h3>';
-													$relatedProfile .= '<span class="bbg__profile-excerpt__occupation">' . $occupation . '</span>';
+													$relatedProfile = '<span class="bbg__profile-excerpt__occupation">' . $occupation . '</span>';
+													$relatedProfile .= '<h3 class="bbg__sidebar__primary-headline"><a href="' . $profileLink . '">' . $profileName . '</a></h3>';
+
 													// $relatedProfile .= '<p class="">' . $profileExcerpt . '</p></div>';
 												}
 
