24ada971aa5208c654ac0ce3cbab5e3d36bf399c
Gigi Frias <gigi.frias@gmail.com>
2016-07-06 16:48:04 -0400

    Updating sidebar on events

--- single-meeting.php
+++ single-meeting.php
@@ -168,6 +168,31 @@ if ( $includeSidebar ) {
 	endif;
 }
 
+// Sidebar multiple downloads drop-down
+$sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
+$sidebarDownloads = "";
+if( $sidebarInclude ) {
+	$downloadsTitle = get_field( 'sidebar_downloads_title' );
+	$optionDefault = get_field ( 'sidebar_downloads_default' );
+	$rows = get_field( 'sidebar_downloads' );
+	if ( $rows ) {
+		$s = '<form style="">';
+		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
+		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
+		$s .= '<option>' . $optionDefault . '</option>';
+
+		foreach( $rows as $row ) {
+			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
+		}
+
+		$s .= '</select>';
+		$s .= '</form>';
+
+		$s .= '<button class="usa-button" id="downloadFile" style="width: 100%;">Download</button>';
+		$sidebarDownloads = $s;
+	}
+}
+
 
 get_header(); ?>
 	<div id="primary" class="content-area">
@@ -364,6 +389,7 @@ get_header(); ?>
 
 						<!-- Additional sidebar content -->
 						<?php
+							echo "<!-- Sidebar content -->";
 							if ( $includeSidebar && $sidebarTitle != "" ) {
 								echo $sidebar;
 							}
