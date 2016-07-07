5099dbfef3000710a8d5cfb82865d200498d40bd
Joe Flowers <jflowers45@gmail.com>
2016-07-07 01:13:04 -0400

    remove unused code

--- page-2-column.php
+++ page-2-column.php
@@ -129,53 +129,6 @@ if( $listsInclude ){
 	}
 }
 
-
-// $sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
-// $sidebarDownloads = "";
-// if( $sidebarInclude ){
-// 	$downloadsTitle = get_field( 'sidebar_downloads_title' );
-// 	$optionDefault = get_field ( 'sidebar_downloads_default' );
-// 	$rows = get_field('sidebar_downloads');
-
-// 	$s = '';
-
-// 	if ( $rows && count($rows) >= 4 ) {
-// 		/* If there are 4 or more downloads, present the list as a dropdown */
-// 		$s = '<form style="">';
-// 		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
-// 		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-// 		$s .= '<option>' . $optionDefault . '</option>';
-
-// 		foreach( $rows as $row ) {
-// 			$fileID = $row['sidebar_download_file']['ID'];
-// 			$file = get_attached_file( $fileID );
-// 			$ext = strtoupper(pathinfo($file, PATHINFO_EXTENSION));
-// 			$filesize = formatBytes(filesize($file));
-// 			$s .= '<option value="' . $row['sidebar_download_file']['url'] .'">' . $row["sidebar_download_title"] . " ($ext, $filesize) " . '</option>';
-// 		}
-
-// 		$s .= '</select>';
-// 		$s .= '</form>';
-// 		$s .= '<button class="usa-button" id="downloadFile" style="width: 100%;">Download</button>';
-// 	} elseif ( $rows ) {
-// 		/* If there are less than 4 downloads, present the downloads as a list */
-// 		$s = '<div>';
-// 		$s .= '<h3>' . $downloadsTitle . '</h3>';
-// 		$s .= '<ul>';
-
-// 		foreach( $rows as $row ) {
-// 			$fileID = $row['sidebar_download_file']['ID'];
-// 			$file = get_attached_file( $fileID );
-// 			$ext = strtoupper(pathinfo($file, PATHINFO_EXTENSION));
-// 			$filesize = formatBytes(filesize($file));
-// 			$s .= '<li><a href="' . $row['sidebar_download_file']['url'] .'">' . $row["sidebar_download_title"] .'</a>' . " ($ext $filesize) " . '</li>';
-// 		}
-
-// 		$s .= '</ul>';
-// 	}
-
-// 	$sidebarDownloads = $s;
-// }
 get_header(); ?>
 
 	<div id="primary" class="content-area">
