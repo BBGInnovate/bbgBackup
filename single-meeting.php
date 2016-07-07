8c964c8e6a2773e3f8b43d609f08283909fb04ea
Joe Flowers <jflowers45@gmail.com>
2016-07-07 01:24:26 -0400

    shared sidebar downloads to one file

--- single-meeting.php
+++ single-meeting.php
@@ -69,32 +69,7 @@ if (in_category("Board Meetings")) {
 $videoUrl = get_post_meta( get_the_ID(), 'featured_video_url', true );
 
 include get_template_directory() . "/inc/shared_sidebar.php";
-
-// Sidebar multiple downloads drop-down
-$sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
-$sidebarDownloads = "";
-if( $sidebarInclude ) {
-	$downloadsTitle = get_field( 'sidebar_downloads_title' );
-	$optionDefault = get_field ( 'sidebar_downloads_default' );
-	$rows = get_field( 'sidebar_downloads' );
-	if ( $rows ) {
-		$s = '<form style="">';
-		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
-		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-		$s .= '<option>' . $optionDefault . '</option>';
-
-		foreach( $rows as $row ) {
-			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
-		}
-
-		$s .= '</select>';
-		$s .= '</form>';
-
-		$s .= '<button class="usa-button" id="downloadFile" style="width: 100%;">Download</button>';
-		$sidebarDownloads = $s;
-	}
-}
-
+include get_template_directory() . "/inc/shared_sidebar_downloads.php";
 
 get_header(); ?>
 	<div id="primary" class="content-area">
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -36,31 +36,9 @@ $twitterURL="//twitter.com/intent/tweet?text=" . rawurlencode( $twitterText );
 $fbUrl="//www.facebook.com/sharer/sharer.php?u=" . urlencode( get_permalink() );
 
 include get_template_directory() . "/inc/shared_sidebar.php";
+include get_template_directory() . "/inc/shared_sidebar_downloads.php";
 
-// Sidebar multiple downloads drop-down
-$sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
-$sidebarDownloads = "";
-if( $sidebarInclude ) {
-	$downloadsTitle = get_field( 'sidebar_downloads_title' );
-	$optionDefault = get_field ( 'sidebar_downloads_default' );
-	$rows = get_field( 'sidebar_downloads' );
-	if ( $rows ) {
-		$s = '<form style="">';
-		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
-		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
-		$s .= '<option>' . $optionDefault . '</option>';
-
-		foreach( $rows as $row ) {
-			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
-		}
-
-		$s .= '</select>';
-		$s .= '</form>';
-
-		$s .= '<button class="usa-button" id="downloadFile" style="width: 100%;">Download</button>';
-		$sidebarDownloads = $s;
-	}
-}?>
+?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("bbg__article"); ?>>
 
