ba0eb572ff72d8ca3889cc0e5728c0cca205c26c
Gigi Frias <gigi.frias@gmail.com>
2016-07-12 16:32:06 -0400

    Folding sidebar download into main sidebar so it shows up again

--- inc/shared_sidebar.php
+++ inc/shared_sidebar.php
@@ -1,5 +1,5 @@
 <?php
-//Include sidebar of Downloads, Links and/or Quotations
+//Include sidebar of single downloads, links, photos, quotations
 $includeSidebar = get_post_meta( get_the_ID(), 'sidebar_include', true );
 if ( $includeSidebar ) {
 
@@ -146,5 +146,30 @@ if ( $includeSidebar ) {
 
 		$sidebar .= $s;
 	endif;
+}
+
+// Sidebar drop-down for multiple downloads
+$sidebarInclude = get_field( 'sidebar_downloads_include', '', true);
+$sidebarDownloads = "";
+if( $sidebarInclude ) {
+	$downloadsTitle = get_field( 'sidebar_downloads_title' );
+	$optionDefault = get_field ( 'sidebar_downloads_default' );
+	$rows = get_field( 'sidebar_downloads' );
+	if ( $rows ) {
+		$s = '<form style="">';
+		$s .= '<label for="options" style="display: inline-block; font-size: 2rem; font-weight: bold; margin-top: 0;">' . $downloadsTitle . '</label>';
+		$s .= '<select name="file_download_list" id="file_download_list" style="display: inline-block;">';
+		$s .= '<option>' . $optionDefault . '</option>';
+
+		foreach( $rows as $row ) {
+			$s .= '<option value="' . $row['sidebar_download_file'] .'">' . $row["sidebar_download_title"] . '</option>';
+		}
+
+		$s .= '</select>';
+		$s .= '</form>';
+
+		$s .= '<button class="usa-button" id="downloadFile" style="width: 100%;">Download</button>';
+		$sidebarDownloads = $s;
+	}
 }
 ?>
