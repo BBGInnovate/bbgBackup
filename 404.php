5ab09681cbcd07d5ddf44919adce9d2127ccbcd3
Brian Williamson <brianwilliamson@brianw.local>
2016-07-13 15:01:05 -0400

    Experimenting with adding a map to the 404 page.

--- 404.php
+++ 404.php
@@ -12,15 +12,27 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+
+				<div class="usa-grid">
+					<header class="page-header">
+						<h1 class="page-title"><span style="color: #900;">404!</span> That page can’t be found.</h1>
+					</header><!-- .page-header -->
+
+					<h6 class="bbg__page-header__tagline">Here are some recent BBG highlights around the world.</h6>
+				</div>
+
+				<!-- this section holds the map and is populated later in the page by javascript -->
+				<section class="usa-section">
+					<div id="map" class="bbg__map--banner" style="max-height: 350px;"></div>
+				</section>
+
+
 			<section class="error-404 not-found usa-grid">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bbginnovate' ); ?></h1>
-				</header><!-- .page-header -->
+
+
+
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bbginnovate' ); ?></p>
-
-					<?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
@@ -54,5 +66,121 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+
+<?php /* include map stuff -------------------------------------------------- */ ?>
+<script src='https://api.tiles.mapbox.com/mapbox.js/v2.2.0/mapbox.js'></script>
+<link href='https://api.tiles.mapbox.com/mapbox.js/v2.2.0/mapbox.css' rel='stylesheet' />
+
+<script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js'></script>
+<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css' rel='stylesheet' />
+<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css' rel='stylesheet' />
+
+<style>
+	.marker-cluster-small {
+		background-color: rgba(255, 255, 255, 0.6) !important;
+	}
+	.marker-cluster-small div {
+		background-color: rgba(255, 0, 0, 0.6) !important;
+	}
+</style>
+
+<script type="text/javascript">
+L.mapbox.accessToken = '<?php echo MAPBOX_API_KEY; ?>';
+
+//console.log(geojson[0].features[0].properties);
+//console.log('description: '+ geojson[0].features[0].properties['description'])
+
+var map = L.mapbox.map('map', 'mapbox.streets')
+//        .setView([-37.82, 175.215], 14);
+
+    var markers = new L.MarkerClusterGroup({
+		iconCreateFunction: function (cluster) {
+			var childCount = cluster.getChildCount();
+			var c = ' marker-cluster-';
+			if (childCount < 10) {
+			    c += 'small';
+			} else if (childCount < 100) {
+			    c += 'medium';
+			} else {
+			    c += 'large';
+			}
+			return new L.DivIcon({ html: '<div><span><b>' + childCount + '</b></span></div>', className: 'marker-cluster' + c, iconSize: new L.Point(40, 40) });
+		}
+	});
+
+    for (var i = 0; i < geojson[0].features.length; i++) {
+        var coords = geojson[0].features[i].geometry.coordinates;
+        var title = geojson[0].features[i].properties.title; //a[2];
+        var description = geojson[0].features[i].properties['description'];
+        var marker = L.marker(new L.LatLng(coords[1], coords[0]), {
+            icon: L.mapbox.marker.icon({
+            	'marker-symbol': '', 
+            	'marker-color': geojson[0].features[i].properties['marker-color']
+           	})
+        });
+        var popupText = title + description;
+        marker.bindPopup(popupText);
+        markers.addLayer(marker);
+    }
+
+    map.addLayer(markers);
+
+	//Disable the map scroll/zoom so that you can scroll the page.
+	map.scrollWheelZoom.disable();
+
+	function centerMap(){
+		map.fitBounds(markers.getBounds());
+	}
+
+
+//console.log(geojson[0].features.length);
+
+/*
+var map = L.mapbox.map('map', 'mapbox.emerald');
+
+var myLayer = L.mapbox.featureLayer().addTo(map);
+	myLayer.setGeoJSON(geojson);
+
+	//Disable the map scroll/zoom so that you can scroll the page.
+	map.scrollWheelZoom.disable();
+
+	function centerMap(){
+		map.fitBounds(myLayer.getBounds());
+	}
+*/
+	centerMap();
+
+
+	//Recenter the map on resize
+	function resizeStuffOnResize(){
+	  waitForFinalEvent(function(){
+			centerMap();
+	  }, 500, "some unique string");
+	}
+
+	//Wait for the window resize to 'end' before executing a function---------------
+	var waitForFinalEvent = (function () {
+		var timers = {};
+		return function (callback, ms, uniqueId) {
+			if (!uniqueId) {
+				uniqueId = "Don't call this twice without a uniqueId";
+			}
+			if (timers[uniqueId]) {
+				clearTimeout (timers[uniqueId]);
+			}
+			timers[uniqueId] = setTimeout(callback, ms);
+		};
+	})();
+
+	window.addEventListener('resize', function(event){
+		resizeStuffOnResize();
+	});
+
+	resizeStuffOnResize();
+</script>
+
+
+
 
 <?php get_footer(); ?>
