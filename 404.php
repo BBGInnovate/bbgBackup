3be752129153f4e6e5476f96e2959ed9240a9f22
Brian Williamson <brianwilliamson@brianw.local>
2016-07-13 15:22:16 -0400

    MINOR.

--- 404.php
+++ 404.php
@@ -36,7 +36,7 @@ get_header(); ?>
 
 					<?php
 						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bbginnovate' ), convert_smilies( ':)' ) ) . '</p>';
+						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives.', 'bbginnovate' ), convert_smilies( ':)' ) ) . '</p>';
 						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 					?>
 
