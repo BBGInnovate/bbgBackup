66acb2d8197ca636f130224429f2c3a053c040d7
Joe Flowers <jflowers45@gmail.com>
2016-07-25 18:42:57 -0400

    profile post cleanup.

--- single-profile.php
+++ single-profile.php
@@ -12,7 +12,6 @@ if ( have_posts() ) {
 
 
 	$metaAuthor = get_the_author();
-	$metaAuthorTwitter = get_the_author_meta( 'twitterHandle' );
 	$ogTitle = get_the_title();
 
 	$metaKeywords = strip_tags(get_the_tag_list('',', ',''));
@@ -86,22 +85,7 @@ get_header(); ?>
 				
 			//the title/headline field, followed by the URL and the author's twitter handle
 			$twitterText = "";
-			$twitterText .= html_entity_decode( get_the_title() );
-			$twitterHandle = get_the_author_meta( 'twitterHandle' );
-			$twitterHandle = str_replace( "@", "", $twitterHandle );
-			if ( $twitterHandle && $twitterHandle != '' ) {
-				$twitterText .= " by @" . $twitterHandle;
-			} else {
-				$authorDisplayName = get_the_author();
-				if ( $authorDisplayName && $authorDisplayName!='' ) {
-					$twitterText .= " by " . $authorDisplayName;
-				}
-			}
-			$twitterText .= " " . get_permalink();
-			$hashtags="";
-			//$hashtags="testhashtag1,testhashtag2";
-
-			///$twitterURL="//twitter.com/intent/tweet?url=" . urlencode(get_permalink()) . "&text=" . urlencode($ogDescription) . "&hashtags=" . urlencode($hashtags);
+			$twitterText .= "Profile of " . html_entity_decode( get_the_title() );
 			$twitterURL="//twitter.com/intent/tweet?text=" . rawurlencode( $twitterText );
 			$fbUrl="//www.facebook.com/sharer/sharer.php?u=" . urlencode( get_permalink() );
 
