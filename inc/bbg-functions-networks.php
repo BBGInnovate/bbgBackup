e9d7b8ef752692e6f4c91782b5f95eeff82aaf9f
Joe Flowers <jflowers45@gmail.com>
2016-07-27 11:40:03 -0400

    temp description for bbg on map

--- inc/bbg-functions-networks.php
+++ inc/bbg-functions-networks.php
@@ -31,6 +31,10 @@ function getNetworkExcerptJS() {
 		}
 	}
 	wp_reset_postdata();
+	$e['bbg'] = array(
+		'description' => 'The mission of the Broadcasting Board of Governors is to inform, engage, and connect people around the world in support of freedom and democracy.',
+		'fullName' => 'Broadcasting Board of Governors'
+	);
 	$s = "<script type='text/javascript'>\n";
 	$s .= "entities=" . json_encode(new ArrayValue($e), JSON_PRETTY_PRINT) . ";";
 	$s .="</script>";
