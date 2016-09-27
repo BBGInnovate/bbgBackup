d9569b1287cc6f4ba1622970dcaf1ed90bf4cee8
Joe Flowers <jflowers45@gmail.com>
2016-09-27 17:23:42 -0400

    add meta description to header

--- header.php
+++ header.php
@@ -79,7 +79,7 @@ $moveUSAbannerBecauseOfAlert = '';
 
 <!-- Mobile Specific Metas
 ================================================== -->
-
+	<meta name="description" content="<?php echo $ogDescription; ?>" />
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
