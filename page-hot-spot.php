31b3ae88b7de8d4ebd8c22a33e7b1f967274faa5
Joe Flowers <jflowers45@gmail.com>
2016-10-19 17:16:34 -0400

    site setting for press index blurb and featured image background position on hot spots

--- page-hot-spot.php
+++ page-hot-spot.php
@@ -17,7 +17,10 @@ $headlineStr = "";
 $featuredImagesData = get_field( 'hot_spot_rotating_featured_images', '', true );
 $randomFeaturedImage = $featuredImagesData[array_rand($featuredImagesData)];
 $featuredImageSrc = $randomFeaturedImage['hot_spot_rotating_featured_image']['sizes']['large-thumb'];
+$featuredImageBackgroundPosition = $randomFeaturedImage['hot_spot_rotating_featured_image_background_position'];
+
 $listsInclude = get_field( 'sidebar_dropdown_include', '', true);
+$pressFreedomIntro = get_field( 'site_setting_press_freedom_intro', 'options', 'false' );
 
 include get_template_directory() . "/inc/shared_sidebar.php";
 
@@ -29,7 +32,7 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class("bbg__article"); ?>>
 						<div class="usa-grid-full">
-							<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__article-header__banner--profile" style="background-image: url(<?php echo $featuredImageSrc; ?>); background-position: center top"></div>
+							<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__article-header__banner--profile" style="background-image: url(<?php echo $featuredImageSrc; ?>); background-position: <?php echo $featuredImageBackgroundPosition; ?>"></div>
 						</div> <!-- usa-grid-full --><!-- .bbg__article-header__thumbnail -->
 						<div class="usa-grid">
 							<header class="entry-header bbg__article-header">
@@ -37,93 +40,97 @@ get_header(); ?>
 									<img src="<?php echo $mapImage['sizes']['medium']; ?>" class="bbg__profile-photo__image">
 								</div>
 								<div class="bbg__profile-title">
-									<h1 class="entry-title bbg__article-header__title">Russia</h1>
+									<h1 class="entry-title bbg__article-header__title"><?php echo get_the_title(); ?></h1>
 									<h5 class="entry-category bbg__profile-tagline">
-										<a href="http://www.voanews.com">Hot Spots</a>									</h5><!-- .bbg__label -->
+										<a href="/hot-spots/">Hot Spots</a>									</h5><!-- .bbg__label -->
 								</div>
 							</header>
 						</div>
-						<div class="readin">
-							<?php
-								the_content();
-							?>
-						</div>
-						<div class="bbg__article-content large">
-							
-							<h2>Challenges</h2>
-							<?php echo $challenges; ?>
-							<h2>Strategic Priorities</h2>
-							<?php echo $priorities; ?>
-
-							<h2 >Languages Served</h2>
-							<table class="usa-table-borderless bbg__jobs__table">
-							<tbody>
-
-							<?php while( have_rows('hot_spot_languages') ): the_row(); ?>
-								<tr>
-									<td><h4><?php the_sub_field('hot_spot_language_name'); ?></h4></td>
-									<td></td>
-								</tr>
-								<?php 
-								if( have_rows('hot_spot_language_sites') ): ?>
-									<?php 
-									while( have_rows('hot_spot_language_sites') ): 
-										the_row();
-										$link = get_sub_field('hot_spot_site_url');
-										$serviceInLanguage = get_sub_field('hot_spot_language_site_name_in_language');
-										$serviceInEnglish = get_sub_field('hot_spot_site_name_in_english');
-										$serviceName = $serviceInLanguage;
-										if ($serviceInEnglish != "") {
-											$serviceName .= " &nbsp;&nbsp;&nbsp;($serviceInEnglish)";
-										}
-									?>
-										<tr>
-											<td><a target="_blank" href="<?php echo $link; ?>" class="bbg__jobs-list__title"><?php echo $serviceName; ?></a></td>
-											<td><?php echo str_replace("http://", "", $link); ?></td>
-										</tr>
-									<?php endwhile; ?>
-								<?php endif; ?>
-							<?php endwhile; ?>
-							</tbody>
-							</table>
-							<h2 >Special Programming</h2>
-							<?php echo $programming; ?>
-
-						</div><!-- .bbg__article-sidebar -->
-						<div class="bbg__article-sidebar large">
-							
-							<div>
-							<?php 
-								if( have_rows('hot_spot_press_freedom_numbers') ):
-									echo '<h5 class="bbg__label small">Press Freedom</h5>';
-									echo '<ul>';
-									while ( have_rows('hot_spot_press_freedom_numbers') ) : the_row();
-										$countryName = get_sub_field('hot_spot_press_freedom_country_name');
-										$freedomIndex = get_sub_field('hot_spot_press_freedom_index');
-										echo "<li>$countryName ($freedomIndex)</li>";
-									endwhile;
-									echo '</ul>';
-								else:
-								endif;
-							?>
-							<aside class="bbg__article-sidebar__aside">
-							<h6 class="bbg__label small"><a href="/threats-to-press/">Threats to Press</a></h6>
-							<ul class="bbg__rss__list">
-							 	<li class="bbg__rss__list-link"><a href="https://bbgredesign.voanews.com/blog/2016/06/08/voa-journalists-attacked-in-turkey/">VOA journalists attacked in Turkey</a></li>
-							 	<li class="bbg__rss__list-link"><a href="https://bbgredesign.voanews.com/blog/2016/05/25/voice-of-america-journalist-arrested-and-beaten-in-angola/">make this dynamic</a></li>
-							</ul>
-							</aside>
-							<h5 class="bbg__label small">By the numbers</h5>
-							<img src="https://placehold.it/300x400" />
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						<div class="usa-grid">
+							<div class="readin">
+								<?php
+									the_content();
+								?>
 							</div>
-							<h5 class="bbg__label small"><a href="https://bbgredesign.voanews.com/our-work/impact-and-results/impact-portfolio/">News from our Networks</a></h5>
-							<article class="bbg__article post-26419 page type-page status-publish has-post-thumbnail hentry"><header class="entry-header bbg-portfolio__excerpt-header ">
-							<div class="single-post-thumbnail clear bbg__excerpt-header__thumbnail--medium " style="margin-bottom: 1rem;"><a tabindex="-1 " href="https://bbgredesign.voanews.com/blog/2016/03/20/the-martis-coverage-reaches-cubans-on-the-island/ "><img class="attachment-small-thumb size-small-thumb wp-post-image " src="https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-300x180.png " sizes="(max-width: 300px) 100vw, 300px " srcset="https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-300x180.png 300w, https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-600x360.png 600w " alt="Two people looking at a computer screen " width="300 " height="180 " /></a></div>
-							<p class=" "><a href="https://bbgredesign.voanews.com/blog/2016/03/20/the-martis-coverage-reaches-cubans-on-the-island/ ">The Martí’s coverage reaches Cubans on the island</a></p>
-							</header><!-- .entry-header -->
-							</article><!-- .bbg-portfolio__excerpt -->
-						</div><!-- .bbg__article-sidebar -->
+
+							<div class="bbg__article-content large">
+								
+								<h2>Challenges</h2>
+								<?php echo $challenges; ?>
+								<h2>Strategic Priorities</h2>
+								<?php echo $priorities; ?>
+
+								<h2 >Languages Served</h2>
+								<table class="usa-table-borderless bbg__jobs__table">
+								<tbody>
+
+								<?php while( have_rows('hot_spot_languages') ): the_row(); ?>
+									<tr>
+										<td><h4><?php the_sub_field('hot_spot_language_name'); ?></h4></td>
+										<td></td>
+									</tr>
+									<?php 
+									if( have_rows('hot_spot_language_sites') ): ?>
+										<?php 
+										while( have_rows('hot_spot_language_sites') ): 
+											the_row();
+											$link = get_sub_field('hot_spot_site_url');
+											$serviceInLanguage = get_sub_field('hot_spot_language_site_name_in_language');
+											$serviceInEnglish = get_sub_field('hot_spot_site_name_in_english');
+											$serviceName = $serviceInLanguage;
+											if ($serviceInEnglish != "") {
+												$serviceName .= " &nbsp;&nbsp;&nbsp;($serviceInEnglish)";
+											}
+										?>
+											<tr>
+												<td><a target="_blank" href="<?php echo $link; ?>" class="bbg__jobs-list__title"><?php echo $serviceName; ?></a></td>
+												<td><?php echo str_replace("http://", "", $link); ?></td>
+											</tr>
+										<?php endwhile; ?>
+									<?php endif; ?>
+								<?php endwhile; ?>
+								</tbody>
+								</table>
+								<h2 >Special Programming</h2>
+								<?php echo $programming; ?>
+
+							</div><!-- .bbg__article-sidebar -->
+							<div class="bbg__article-sidebar large">
+								
+								<div>
+								<?php 
+									if( have_rows('hot_spot_press_freedom_numbers') ):
+										echo '<h5 class="bbg__label small">Press Freedom</h5>';
+										echo $pressFreedomIntro;
+										echo '<ul>';
+										while ( have_rows('hot_spot_press_freedom_numbers') ) : the_row();
+											$countryName = get_sub_field('hot_spot_press_freedom_country_name');
+											$freedomIndex = get_sub_field('hot_spot_press_freedom_index');
+											echo "<li>$countryName ($freedomIndex)</li>";
+										endwhile;
+										echo '</ul>';
+									else:
+									endif;
+								?>
+								<aside class="bbg__article-sidebar__aside">
+								<h6 class="bbg__label small"><a href="/threats-to-press/">Threats to Press</a></h6>
+								<ul class="bbg__rss__list">
+								 	<li class="bbg__rss__list-link"><a href="https://bbgredesign.voanews.com/blog/2016/06/08/voa-journalists-attacked-in-turkey/">VOA journalists attacked in Turkey</a></li>
+								 	<li class="bbg__rss__list-link"><a href="https://bbgredesign.voanews.com/blog/2016/05/25/voice-of-america-journalist-arrested-and-beaten-in-angola/">make this dynamic</a></li>
+								</ul>
+								</aside>
+								<h5 class="bbg__label small">By the numbers</h5>
+								<img src="https://placehold.it/300x400" />
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+								</div>
+								<h5 class="bbg__label small"><a href="https://bbgredesign.voanews.com/our-work/impact-and-results/impact-portfolio/">News from our Networks</a></h5>
+								<article class="bbg__article post-26419 page type-page status-publish has-post-thumbnail hentry"><header class="entry-header bbg-portfolio__excerpt-header ">
+								<div class="single-post-thumbnail clear bbg__excerpt-header__thumbnail--medium " style="margin-bottom: 1rem;"><a tabindex="-1 " href="https://bbgredesign.voanews.com/blog/2016/03/20/the-martis-coverage-reaches-cubans-on-the-island/ "><img class="attachment-small-thumb size-small-thumb wp-post-image " src="https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-300x180.png " sizes="(max-width: 300px) 100vw, 300px " srcset="https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-300x180.png 300w, https://bbgredesign.voanews.com/wp-content/media/2016/03/Screen-Shot-2016-03-20-at-8.45.01-PM-600x360.png 600w " alt="Two people looking at a computer screen " width="300 " height="180 " /></a></div>
+								<p class=" "><a href="https://bbgredesign.voanews.com/blog/2016/03/20/the-martis-coverage-reaches-cubans-on-the-island/ ">The Martí’s coverage reaches Cubans on the island</a></p>
+								</header><!-- .entry-header -->
+								</article><!-- .bbg-portfolio__excerpt -->
+							</div><!-- .bbg__article-sidebar -->
+						</div>
 					</article><!-- #post-## -->
 				<?php endwhile; // End of the loop. ?>
 			</div><!-- .usa-grid-full -->
