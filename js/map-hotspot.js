4037fc25c70cb467c637521e83037f08dc2fa189
Joe Flowers <jflowers45@gmail.com>
2016-11-07 23:29:50 -0500

    hot spot map revision

--- js/map-hotspot.js
+++ js/map-hotspot.js
@@ -1,3 +1,4 @@
+var COLOR_HOVER = "#CC0000";
 var colors = {
 	iran: "#83c2ba",
 	russia: "#ebdb8b",
@@ -6,31 +7,34 @@ var colors = {
 	cve: "#C9B7AD"
 }
 var spheres = {
-	cve: {
-		comprisedOf: ['Russia'],
-		influences: [
-			'Afghanistan','Armenia','Azerbaijan','Bahrain','Bangladesh','Brunei','Cambodia','Taiwan','East Timor','Indonesia','Iran','Iraq','Israel','Palestine','Japan','Jordan','Kazakhstan','Kuwait','Kyrgyzstan','Laos','Lebanon','Malaysia','Maldives','Myanmar','North Korea','Oman','Pakistan','Philippines','Qatar','Russia','Saudi Arabia','Singapore','South Korea','Sri Lanka','Syria','Tajikistan','Thailand','Taiwan','Turkey','Turkmenistan','United Arab Emirates','Uzbekistan','Vietnam','Yemen',
-			'Papua New Guinea',
-			'Albania','Andorra','Austria','Belarus','Belgium','Bosnia and Herzegovina','Bulgaria','Croatia','Cyprus','Czech Republic','Denmark','Estonia','Finland','France','Georgia','Germany','Greece','Hungary','Ireland','Italy','Latvia','Liechtenstein','Lithuania','Luxembourg','Republic of Macedonia','Malta','Moldova','Monaco','Montenegro','Netherlands','Norway','Poland','Portugal','Romania','Russia','San Marino','Serbia','Slovakia','Slovenia','Spain','Sweden','Switzerland','Turkey','Ukraine','United Kingdom','Vatican City',
-			'Egypt','Morocco','Libya','Algeria','Mali','Mauritania', 'Chad', 'Western Sahara', 'Sudan','Eritrea','Ethiopia','Somalia', 'Cameroon', 'CÃ´te d\'Ivoire', 'Benin', 'Ghana','Togo', 'Liberia', 'Niger', 'Burkina Faso', 'Senegal', 'Nigeria', 'Guinea', 'Sierra Leone', 'Gambia'
-		],
-		color: colors['cve'],
-		color2: colors['cve'],
-		label: "Countering Violent Extremism"
-	},
 	iran: {
 		comprisedOf: ['Iran'],
 		influences: ['Armenia','Azerbaijan','Turkey','Syria','Jordan','Israel','Afghanistan','Pakistan','Cyprus'],
 		color: colors['iran'],
-		color2: colors['iran'],
 		label: "Iran"
 	},
 	russia: {
 		comprisedOf: ['Russia', 'Svalbard and Jan Mayen'],
 		influences: ['Estonia','Latvia','Lithuania','Belarus','Moldova','Syria','Kazakhstan','Uzbekistan','Turkmenistan','Tajikistan','Kyrgyzstan','Azerbaijan','Armenia','Georgia'],
 		color: colors['russia'],
-		color2: colors['russia'],
 		label: "Russia"
+	},
+	cuba: {
+		comprisedOf: ['Cuba'],
+		influences: ['Venezuela','Colombia','Ecuador','Peru','Bolivia'],
+		color: colors['cuba'],
+		label: "Cuba"
+	},
+	cve: {
+		comprisedOf: ['Russia'],
+		influences: [
+			'Afghanistan','Armenia','Azerbaijan','Bahrain','Bangladesh','Brunei','Cambodia','Taiwan','East Timor','Indonesia','Iran','Iraq','Israel','Palestine','Japan','Jordan','Kazakhstan','Kuwait','Kyrgyzstan','Laos','Lebanon','Maldives','Myanmar','North Korea','Oman','Pakistan','Philippines','Qatar','Russia','Saudi Arabia','Singapore','South Korea','Sri Lanka','Syria','Tajikistan','Thailand','Taiwan','Turkey','Turkmenistan','United Arab Emirates','Uzbekistan','Vietnam','Yemen',
+			'Papua New Guinea',
+			'Albania','Andorra','Belarus','Bosnia and Herzegovina','Bulgaria','Croatia','Cyprus','Czech Republic','Denmark','Estonia','Finland','Georgia','Greece','Hungary','Latvia','Lithuania','Republic of Macedonia','Malta','Moldova','Montenegro','Norway','Poland','Romania','Russia','San Marino','Serbia','Slovakia','Slovenia','Sweden','Turkey','Ukraine','Vatican City',
+			'Egypt','Morocco','Libya','Algeria','Mali','Mauritania', 'Chad', 'Western Sahara', 'Sudan','Eritrea','Ethiopia','Somalia', 'Cameroon', 'CÃ´te d\'Ivoire', 'Benin', 'Ghana','Togo', 'Liberia', 'Niger', 'Burkina Faso', 'Senegal', 'Nigeria', 'Guinea', 'Sierra Leone', 'Gambia'
+		],
+		color: colors['cve'],
+		label: "Countering Violent Extremism"
 	},
 	china: {
 		comprisedOf: ['China'],
@@ -39,33 +43,38 @@ var spheres = {
 			'Angola','Benin','Botswana','Burkina Faso','Burundi','Cameroon','Cape Verde','Central African Republic','Chad','Comoros','Republic of Congo','Democratic Republic of Congo','CÃ´te d\'Ivoire','Djibouti','Equatorial Guinea','Eritrea','Ethiopia','Gabon','Gambia','Ghana','Guinea','Guinea-Bissau','Kenya','Lesotho','Liberia','Madagascar','Malawi','Mali','Mauritania','Mauritius','Mozambique','Namibia','Niger','Nigeria','Rwanda','São Tomé and Príncipe','Senegal','Seychelles','Sierra Leone','Somalia','South Africa','South Sudan','Sudan','Swaziland','Tanzania','Togo','Uganda','Western Sahara','Zambia','Zimbabwe',
 			'Costa Rica','El Salvador','Guatemala','Honduras','Mexico','Nicaragua','Panama'],
 		color: colors['china'],
-		color2: colors['china'],
 		label: "China"
 	},
-	cuba: {
-		comprisedOf: ['Cuba'],
-		influences: ['Venezuela','Colombia','Ecuador','Peru','Bolivia'],
-		color: colors['cuba'],
-		color2: colors['cuba'],
-		label: "Cuba"
-	},
 };
-fullCountryList = AmCharts.maps.worldLow.svg.g.path;
-cMap = {};
+var fullCountryList = AmCharts.maps.worldLow.svg.g.path;
+var activeSphere = "all";
+
+cMap = {}; //this allows us to look a country's ID up from its title, so that the data can originally be entered as country titles
+cMapByID = {};
 for (var i = 0; i < fullCountryList.length; i++) {
 	c = fullCountryList[i];
+	c.spheres = [];
 	cMap[c.title] = c.id;
+	cMapByID[c.id] = c;
 }
-sMap = {};
+
+sMap = {};  //this maps countryIDs to spheres so that when you click a country, it takes you to a sphere.
+sAreas = {};
 for (var key in spheres) {
 	if (spheres.hasOwnProperty(key)) {
 		s = spheres[key];
+		sAreas[key] = [];
 		var sphereCountries = s.comprisedOf.concat(s.influences);
 		for (var i=0; i < sphereCountries.length; i++) {
 			var countryName = sphereCountries[i];
 			if (cMap.hasOwnProperty(countryName)) {
 				var countryID = cMap[countryName];
-				sMap[countryID] = key;
+				sAreas[key].push(countryID);
+				console.log("pusing " + countryID + " onto " + key);
+				if (! (sMap.hasOwnProperty(countryID))) {
+					sMap[countryID] = key;	
+				}
+				cMapByID[countryID].spheres.push(key);
 			}
 		}
 	}
@@ -80,48 +89,71 @@ jQuery( document ).ready(function() {
     jQuery('.cve').css('background-color', colors['cve']);
 
     jQuery('#mapFilters input').click(function(e) {
-    	var selectedSphere = jQuery(this).val();
-    	setActiveSphere(selectedSphere);
+    	setActiveSphere(jQuery(this).val());
     });
 
 
 });
 
-function getAreas(selectedSphere) {
+function getAreas(aSphere) {
 	var areas = [];
-	for (var key in spheres) {
-		if (spheres.hasOwnProperty(key)) {
-			s = spheres[key];
-			if (selectedSphere == "all" || selectedSphere == key) {
-				var sphereCountries = s.comprisedOf.concat(s.influences);
-				for (var i=0; i < sphereCountries.length; i++) {
-					var countryName = sphereCountries[i];
-					if (cMap.hasOwnProperty(countryName)) {
-						var countryID = cMap[countryName];
-						var a = {
-							id: countryID,
-							title: countryName,
-							color: s.color,
-							groupId: key,
-							customData:s.label
-						}
-						areas.push(a);
-					} else {
-						console.log("no match for " + countryName);
+	
+	if (aSphere == "all") {
+		var areas = [];
+		for (var i = 0; i < fullCountryList.length; i++) {
+			c = fullCountryList[i];
+			if (sMap[c.id]) {
+				var a = {
+					id: c.id,
+					title: c.title,
+					color: colors[c.spheres[0]]
+				}
+				areas.push(a);	
+			}
+		}
+	} else {
+		for (var key in spheres) {
+			if (spheres.hasOwnProperty(key)) {
+				s = spheres[key];
+				if (aSphere == key) {
+					var sphereCountries = s.comprisedOf.concat(s.influences);
+					for (var i=0; i < sphereCountries.length; i++) {
+						var countryName = sphereCountries[i];
+						if (cMap.hasOwnProperty(countryName)) {
+							var countryID = cMap[countryName];
+							var a = {
+								id: countryID,
+								title: countryName,
+								color: s.color,
+								customData:s.label
+							}
+							areas.push(a);
+						} 
 					}
 				}
 			}
 		}
 	}
+
+	
 	return areas;
 }
 
 function setActiveSphere(s) {
-	map.dataProvider.areas = getAreas(s);
+	activeSphere=s;
+	var newAreas = getAreas(s);
+	map.dataProvider.areas = newAreas;
 	map.validateData();
+	
+	// var g2 = [];
+	// for (var i=0; i < sAreas[activeSphere].length; i++) {
+	// 	var id2 = sAreas[activeSphere][i];
+	// 	var o = map.getObjectById(id2);
+	// 	g2.push(o);
+	// }
+	// map.zoomToGroup(g2);
+
 }
-
-
 
 (function ($,bbgConfig, entities) {
 
@@ -133,42 +165,101 @@ function setActiveSphere(s) {
 			type: "map",
 			dataProvider: {
 				map: "worldLow",
-				areas:getAreas('all')
+				areas:getAreas("all")
 			},
 			areasSettings: {
-				autoZoom: false,
-				selectable:true,
-					"rollOverOutlineColor": "#FFFFFF",
-					"rollOverColor": "#CC0000",
-					"alpha": 1,
-					"unlistedAreasAlpha": 0.6,
-					"balloonText": "[[customData]] Hot Spot"
+				autoZoom: true,
+				alpha: 1,
+				unlistedAreasAlpha: 0.6,
+				color:"#CCCCCC",
+				selectable: true
 			},
 		} );
-
+		
 		map.addListener("clickMapObject", function (event) {
 			sphere = sMap[event.mapObject.id];
 			window.location = bbgConfig.template_directory_uri + "../../../hot-spots/" + sphere;
+		});
+
+		map.balloonLabelFunction = function (area, map) {      
+			var txt = "";
+			if (activeSphere != "all") {
+				txt= spheres[activeSphere].label + " Hot Spot";
+			} else {
+				var sphere = spheres[sMap[area.id]];
+		    	if (sphere) {
+		    		txt = sphere.label + " Hot Spot";
+		    	}	
+			}
+			
+	    	return txt;
+	    };
+
+		map.addListener("rollOutMapObject", function (event) {
+			var countryID = event.mapObject.id;
+			var primarySphere = sMap[countryID];
+			if (primarySphere) {
+				if (activeSphere != "all") {
+//					console.log("mismatch2 " + primarySphere + " and " +activeSphere);
+					primarySphere = activeSphere;
+				}
+				var c = cMapByID[countryID];
+				event.mapObject.color = colors[c.spheres[0]];
+				event.mapObject.validate();
+				var s = spheres[primarySphere];
+				var sphereCountries = s.comprisedOf.concat(s.influences);
+				for (var i=0; i < sphereCountries.length; i++) {
+					var countryName = sphereCountries[i];
+					if (cMap.hasOwnProperty(countryName)) {
+						var countryID = cMap[countryName];
+						var c2 = cMapByID[countryID];
+						var mapObject = map.getObjectById(countryID);
+						if (mapObject) {
+							
+							if (activeSphere == "all") {
+								mapObject.color = colors[c2.spheres[0]];
+								mapObject.validate();
+							} else {
+								mapObject.color = colors[activeSphere];
+								mapObject.validate();
+							}
+
+						}
+					}
+				}
+			}
+			
+		});
+		map.addListener("rollOverMapObject", function (event) {
+			var countryID = event.mapObject.id;
+			var primarySphere = sMap[countryID];
+
+			if (primarySphere) {
+				if (activeSphere != "all") {
+					//console.log("mismatch " + primarySphere + " and " +activeSphere);
+					primarySphere = activeSphere;
+				}
+				var c = cMapByID[countryID];
+				event.mapObject.color = COLOR_HOVER;
+				event.mapObject.validate();
+				
+				//loop through all countries that are in this country's primary sphere
+				var s = spheres[primarySphere];
+				var sphereCountries = s.comprisedOf.concat(s.influences);
+				for (var i=0; i < sphereCountries.length; i++) {
+					var countryName = sphereCountries[i];
+					if (cMap.hasOwnProperty(countryName)) {
+						var countryID = cMap[countryName];
+						var mapObject = map.getObjectById(countryID);
+						if (mapObject) {
+							mapObject.color = COLOR_HOVER;
+							mapObject.validate();
+						}
+					}
+				}
+			}
 		});
 	});
 
 
 })(jQuery,bbgConfig, entities);
-
-		// for (var i=0; i < s.influences.length; i++) {
-		// 	var countryName = s.influences[i];
-		// 	if (cMap.hasOwnProperty(countryName)) {
-		// 		var countryID = cMap[countryName];
-		// 		sMap[countryID] = key;
-		// 		var a = {
-		// 			id: countryID,
-		// 			title: countryName,
-		// 			color: s.color2,
-		// 			groupId: key,
-		// 			customData:s.label
-		// 		}
-		// 		areas.push(a);
-		// 	} else {
-		// 		console.log("no match for " + countryName);
-		// 	}
-		// }
