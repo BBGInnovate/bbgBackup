5dcc031b6da603aeae3b467870801270fc4cb9cd
Joe Flowers <jflowers45@gmail.com>
2016-11-14 20:24:27 -0500

    typo

--- page-kits.php
+++ page-kits.php
@@ -535,19 +535,19 @@ get_header(); ?>
 								,'cat' => get_cat_ID('Award')
 								,'posts_per_page' => 3
 								,'orderby' => 'post_date'
-								,'order' => 'desc',
+								,'order' => 'desc'
 							);
 							$custom_query_args= $qParams;
 							$custom_query = new WP_Query( $custom_query_args );
 							echo '<h6 class="bbg__label"><a href="' . get_category_link( get_cat_ID('Award') ) . '">Recent Awards</a></h6>';
-						
+
 							echo '<div class=" usa-grid">';
 							while ( $custom_query->have_posts() )  {
 								$custom_query->the_post();
 								$gridClass = "bbg-grid--1-2-3";
 								get_template_part( 'template-parts/content-portfolio', get_post_format() );
 							}
-							echo "</div>"
+							echo "</div>";
 							wp_reset_query();
 						endif;
 					endwhile;
