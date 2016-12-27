8a555cc7c534e8c5b5c13371b46ea15c39e0b20c
Joe Flowers <jflowers45@gmail.com>
2016-12-27 18:45:33 -0500

    remove legacy eventbrite code (we now use url and not ID)

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -101,13 +101,6 @@ if ( $includeMap && $mapLocation) {
 	//$map = "https://api.mapbox.com/v4/mapbox.emerald/" . $pin . $lng . ",". $lat . "," . $zoom . "/170x300.png?access_token=" . $key;
 }
 
-/*** For Events, there is often an eventBriteID that has been entered - render the iFrame ***/
-$eventBriteID = get_post_meta( get_the_ID(), 'board_meeting_eventbrite_id', true );
-$eventStr="";
-if ($eventBriteID) {
-	$eventStr='<div style="width:100%; text-align:left;" ><iframe  src="//eventbrite.com/tickets-external?eid='.$eventBriteID.'&ref=etckt" frameborder="0" height="260" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a class="powered-by-eb" style="color: #dddddd; text-decoration: none;" target="_blank" href="http://www.eventbrite.com/r/etckt">Powered by Eventbrite</a></div></div>';
-}
-
 // Include sidebar list of people who worked on the project
 $teamRoster = "";
 if( have_rows('project_team_members') ):
@@ -574,7 +567,6 @@ $hideFeaturedImage = FALSE;
 
 		<div class="entry-content bbg__article-content <?php echo $featuredImageClass; ?>">
 			<?php
-				echo $eventStr;
 				echo $pageContent;
 
 				/* START AWARD INFO */
