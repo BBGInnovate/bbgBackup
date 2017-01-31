081ea27b2881979f67740f8a86fc50fd2bbd2284
Joe Flowers <jflowers45@gmail.com>
2017-01-31 17:28:39 -0500

    typo

--- inc/bbg-custom-post-types-and-taxonomies.php
+++ inc/bbg-custom-post-types-and-taxonomies.php
@@ -73,7 +73,7 @@ function register_custom_post_types() {
 		'public'                => false,
 		'show_ui'	        => true,
 		'show_in_admin_bar'     => true
-		'menu_position' => 5,
+		'menu_position' => 5
 		//'show_in_menu' => 'edit.php',
 	);
 	register_post_type( 'threat_to_press', $args );
