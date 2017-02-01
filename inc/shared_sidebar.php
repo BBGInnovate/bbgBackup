400926100c63d488d229407ee6a9786fa4ad5416
Joe Flowers <jflowers45@gmail.com>
2017-02-01 12:26:01 -0500

    typo

--- inc/shared_sidebar.php
+++ inc/shared_sidebar.php
@@ -208,7 +208,7 @@ if ( $includeSidebar ) {
 				$s .= '<a data-chrome="noheader" class="twitter-timeline"  href="https://twitter.com/hashtag/' . $widgetHashtag . '" data-widget-id="' . $widgetID . '">#' . $widgetHashtag . ' Tweets</a>';
 				$s .= '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?';
 				$s .= "'http':'https'";
-				$s .= ';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
+				$s .= ';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
           
 			}
 		endwhile;
