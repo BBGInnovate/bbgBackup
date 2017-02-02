1ca7896fa1b085788397427efd863f1d0594e78d
Joe Flowers <jflowers45@gmail.com>
2017-02-01 22:43:42 -0500

    sortable status on threats

--- inc/bbg-custom-post-types-and-taxonomies.php
+++ inc/bbg-custom-post-types-and-taxonomies.php
@@ -86,6 +86,7 @@ add_action( 'init', 'register_custom_post_types' );
 /************ Add a few columns to the admin view ************/
 add_filter('manage_threat_to_press_posts_columns', 'ttp_table_head');
 function ttp_table_head( $defaults ) {
+    $defaults['threats_to_press_status']  = 'Status';
     $defaults['threats_to_press_country']  = 'Country';
     $defaults['threats_to_press_network']    = 'Network(s)';
     $defaults['threats_to_press_link']    = 'Link';
@@ -102,6 +103,10 @@ function ttp_table_content( $column_name, $post_id ) {
 	if ($column_name == 'threats_to_press_target_names') {
 		$targetName = get_post_meta( $post_id, 'threats_to_press_target_names', true );
 		echo $targetName;
+	}
+	if ($column_name == 'threats_to_press_status') {
+		$status = get_field_object('threats_to_press_status', $post_id, true );
+		echo $status['choices'][$status['value']];
 	}
 	if ($column_name == 'threats_to_press_network') {
 		//$network = get_post_meta( $post_id, 'threats_to_press_network', true );
@@ -131,6 +136,7 @@ function ttp_table_content( $column_name, $post_id ) {
 add_filter( 'manage_edit-threat_to_press_sortable_columns', 'sortableThreatsCols' );
 function sortableThreatsCols( $sortable_columns ) {
    $sortable_columns[ 'threats_to_press_country' ] = 'threats_to_press_country';
+   $sortable_columns[ 'threats_to_press_status' ] = 'threats_to_press_status';
    return $sortable_columns;
 }
 add_action( 'pre_get_posts', 'ttp_orderby' );
@@ -141,6 +147,10 @@ function ttp_orderby( $query ) {
     $orderby = $query->get( 'orderby');
     if( 'threats_to_press_country' == $orderby ) {
         $query->set('meta_key','threats_to_press_country');
+        $query->set('orderby','meta_value');
+    }
+    if( 'threats_to_press_status' == $orderby ) {
+        $query->set('meta_key','threats_to_press_status');
         $query->set('orderby','meta_value');
     }
 }
