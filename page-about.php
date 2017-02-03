58768c3284939917040facba142f852a9ce52438
Gigi Frias <gigi.frias@gmail.com>
2017-02-03 15:47:00 -0500

    finding the issue

--- page-about.php
+++ page-about.php
@@ -95,7 +95,7 @@ get_header();
 			<div id="page-children" class="usa-section usa-grid bbg__about__children">
 			<?php
 				// check if the flexible content field has rows of data
-				if ( have_rows('about_flexible_page_rows') ):
+				if ( have_rows( 'about_flexible_page_rows' ) ):
 					$counter = 0;
 					$pageTotal = 1;
 					$containerClass = 'bbg__about__child ';
@@ -111,26 +111,27 @@ get_header();
 
 						if ( get_row_layout() != 'about_ribbon_page' ) { // Check if row is a ribbon
 							echo '<!-- ROW ' . $counter . '-->'; // Add row counter
-							echo '<section class="usa-grid-full bbg__about__children--row">'; // Wrap row in usa-grid container
+							echo '<section class="usa-grid-full bbg__about__children--row">'; // Open row
 						} else {
 							echo '<!-- ROW ' . $counter . '-->'; // Add row counter
-							echo '<section class="usa-grid-full bbg__about__children--row bbg__ribbon--thin">'; // Add ribbon row class
+							echo '<section class="usa-grid-full bbg__about__children--row bbg__ribbon--thin">'; // Open row and add ribbon class
 						}
 
 						if ( get_row_layout() == 'about_multicolumn' ):
 
 							/*** BEGIN DISPLAY OF ENTIRE MULTICOLUMN ROW ***/
 							$relatedPages = get_sub_field( 'about_muliticolumn_related_pages' );
-							$pageTotal = count ( $relatedPages );
+							$pageTotal = count ( $relatedPages ); // count total number of pages
 							$largeNumber = FALSE;
 
-							if ( $pageTotal == 5 ) {
-								$five = TRUE;
+							if ( $pageTotal == 5 ) { // if number of pages is 5
+								$five = TRUE; // set variable to true
 							} else {
-								$five = FALSE;
+								$five = FALSE; // else set to false
 							}
 
-							foreach ( $relatedPages as $rPage ) {
+							foreach ( $relatedPages as $rPage ) { // loop through each field in the $relatedPages array
+								// Set variables
 								$rPageHeadline = $rPage -> headline;
 								$rPageTagline = $rPage -> page_tagline;
 								$rPageDescription = $rPage -> about_include_exerpt;
@@ -145,14 +146,15 @@ get_header();
 									'post__in' => array( $rPage -> ID )
 								);
 
-								query_posts( $qParams ); // Run query
+								// Run query
+								query_posts( $qParams );
 
-								if ( have_posts() ) {
-									while ( have_posts() ) {
+								if ( have_posts() ) { // if there are posts
+									while ( have_posts() ) { // loop through all the posts
 										the_post();
 
-										if ( get_the_title() == "Mission" ) {
-											$pageTotal = $pageTotal - 1; // Lower page count if Mission page included
+										if ( get_the_title() == "Mission" ) { // check for Mission page
+											$pageTotal = $pageTotal - 1; // Lower page count if Mission page exists
 										}
 
 										// Check if total number of pages is odd or even
@@ -163,9 +165,9 @@ get_header();
 											// if FALSE: number is even
 											$gridClass = 'bbg-grid--1-2-2'; // add 2-col grid class
 										}
-										$grids = $gridClass; // pass grid class variable
+										$grids = $gridClass; // set grid class variable
 
-										$specialMissionClass = $five; // change mission dimensions when there are 5 items
+										$specialMissionClass = $five; // change mission class when there are 5 items
 
 										$headline = $rPageHeadline; // custom field for secondary page headline
 										$tagline = $rPageTagline; // custom field for page tagline
@@ -174,20 +176,20 @@ get_header();
 										get_template_part( 'template-parts/content-about', get_post_format() );
 
 										if ( get_the_title() == "Mission" ) { // Close Mission row if present
-											echo "</section>";
-											echo '<section class="usa-grid-full bbg__about__children--row">';
+											echo '</section>';
+											echo '<section class="usa-grid-full bbg__about__children--row">'; // open new row
 										}
 									}
 								}
 								wp_reset_query();
 							}
-						echo '</section>';
+						echo '</section>'; // close row
 						/*** END DISPLAY OF ENTIRE MULTICOLUMN ROW ***/
 
 						elseif ( get_row_layout() == 'about_umbrella_page' ):
 						/*** BEGIN DISPLAY OF ENTIRE UMBRELLA ROW ***/
 							$umbrellaPages = get_sub_field( 'about_umbrella_related_pages' );
-							$pageTotal = count ( $umbrellaPages ); // count page total
+							$pageTotal = count ( $umbrellaPages ); // count subpages
 
 							// Check if page count is even or odd
 							if ( checkNum($pageTotal) === TRUE ) {
@@ -202,23 +204,23 @@ get_header();
 							$labelLink = get_sub_field('about_umbrella_label_link');
 							$introText = get_sub_field('about_umbrella_intro_text');
 
-							//allow shortcodes in intro text
+							// allow shortcodes in intro text
 							$introText = apply_filters( 'the_content', $introText );
 							$introText = str_replace( ']]>', ']]&gt;', $introText );
 
-							if ($labelLink) { // if the label has a URL add link and right arrow
+							if ( $labelLink ) { // if the label has a URL add link and right arrow
 								echo '<h6 class="bbg__label"><a href="$labelLink">$labelText</a> <span class="bbg__links--right-angle-quote" aria-hidden="true">&raquo;</span></h6>';
 							} else { // else only show label
 								echo '<h6 class="bbg__label">$labelText</h6>';
 							}
 
 							// Output umbrella section intro text
-							if ($introText) {
+							if ( $introText ) {
 								echo '<div class="bbg__about__child__intro">$introText</div>';
 							}
 
 							// Output grandchild pages (subpages)
-							echo '<div class="usa-grid-full bbg__about__grandchildren">';
+							echo '<div class="usa-grid-full bbg__about__grandchildren">'; // open grandchildren container
 								if ( $umbrellaPages ) {
 									// Loop through all the grandchild pages
 									foreach ( $umbrellaPages as $rp ) {
@@ -242,14 +244,14 @@ get_header();
 											}
 
 											if ( $thumbSrc ) { // Add thumbnail if set
-												echo '<a href="$url"><div class="bbg__about__grandchild__thumb" style="background-image: url( . $thumbSrc[0]' .  '); background-position:'  . $thumbPosition . ';"></div></a>';
+												echo '<a href="$url"><div class="bbg__about__grandchild__thumb" style="background-image: url(' . $thumbSrc[0] .  '); background-position:'  . $thumbPosition . ';"></div></a>';
 											}
 											echo $excerpt; // Output page excerpt
 										echo '</article>';
 									}
 								}
-							echo '</div>';
-						echo '</section>';
+							echo '</div>'; // close grandchildren container
+						echo '</section>'; // close row
 						/*** END DISPLAY OF ENTIRE UMBRELLA ROW ***/
 
 						elseif ( get_row_layout() == 'about_downloads_files' ):
@@ -267,7 +269,7 @@ get_header();
 							}
 
 							$downloadsLabel = get_sub_field( 'about_downloads_label' );
-							$downloadsIntro = get_sub_field('about_downloads_description');
+							$downloadsIntro = get_sub_field( 'about_downloads_description' );
 
 							// Allow shortcodes in intro text
 							$downloadsIntro = apply_filters( 'the_content', $downloadsIntro );
@@ -281,9 +283,9 @@ get_header();
 							echo '<div class="bbg__about__child__intro">$downloadsIntro</div>';
 
 							// Output grandchild pages (subpages)
-							echo '<div class="usa-grid-full bbg__about__grandchildren">';
+							echo '<div class="usa-grid-full bbg__about__grandchildren">'; // open granchildren container
 								if ( $downloadFiles ) {
-									// Loop through all the grandchild pages
+									// Loop through all the grandchild pages/files
 									foreach ($downloadFiles as $file) {
 										// Define all variables
 										$fileName = $file['downloads_link_name'];
@@ -296,7 +298,7 @@ get_header();
 
 										// Create array from file object
 										$fileObj = $file['downloads_file'];
-											// Define variables
+											// Define variables from array fields
 											$fileID = $fileObj['ID'];
 											$fileURL = $fileObj['url'];
 											$file = get_attached_file( $fileID );
@@ -304,7 +306,7 @@ get_header();
 											$fileSize = formatBytes( filesize( $file ) ); // file size
 
 										// Output variables
-										echo '<article class="$containerClass bbg__about__grandchild">';
+										echo '<article class="' . $containerClass . ' bbg__about__grandchild">';
 											// Output file title/name
 											echo '<h3 class="bbg__about__grandchild__title"><a href="' . $fileURL . '" target="_blank">' . $fileName . '</a> <span class="bbg__file-size">(' . $fileExt . ', ' . $fileSize . ')</span></h3>';
 											// Output file thumbnail
@@ -313,15 +315,15 @@ get_header();
 													echo '<div class="bbg__about__grandchild__thumb" style="background-image: url(' . $thumbSrc[0] . ');"></div>';
 												echo '</a>';
 											}
-											// Output file description
+											// Output file description if set
 											if ( $fileDescription ) {
 												echo $fileDescription;
 											}
 										echo '</article>';
 									}
 								}
-							echo '</div>';
-						echo '</section>';
+							echo '</div>'; // close granchildren container
+						echo '</section>'; // close row
 						/*** END DISPLAY OF DOWNLOAD LINKS ROW ***/
 
 						elseif( get_row_layout() == 'about_ribbon_page' ):
@@ -339,30 +341,30 @@ get_header();
 							$summary = str_replace( ']]>', ']]&gt;', $summary );
 
 							echo '<div class="usa-grid">';
-								echo '<div class="bbg__announcement__flexbox">';
+								echo '<div class="bbg__announcement__flexbox" name="' . $labelText . '">'; // open ribbon container and set div name to $labelText
 
 									if ( $imageURL ) { // Output image thumbnail if set
 										echo '<div class="bbg__announcement__photo" style="background-image: url($imageURL);"></div>';
 									}
 
-									echo '<div>';
+									echo '<div>'; // Open ribbon text container
 										if ( $labelLink ) { // Output label with link if set
-											echo '<h6 class="bbg__label"><a href="' . get_permalink($labelLink) . '">$labelText</a></h6>';
+											echo '<h6 class="bbg__label"><a href="' . get_permalink($labelLink) . '">' . $labelText . '</a></h6>';
 										} else { // Else output link only
-											echo '<h6 class="bbg__label">$labelText</h6>';
+											echo '<h6 class="bbg__label">' . $labelText . '</h6>';
 										}
 
 										if ( $headlineLink ) { // Output headline with link if set
-											echo '<h2 class="bbg__announcement__headline"><a href="' . get_permalink($headlineLink) . '">$headlineText</a></h2>';
+											echo '<h2 class="bbg__announcement__headline"><a href="' . get_permalink($headlineLink) . '">' . $headlineText . '</a></h2>';
 										} else { // Else output headline only
-											echo '<h2 class="bbg__announcement__headline">$headlineText</h2>';
+											echo '<h2 class="bbg__announcement__headline">' . $headlineText . '</h2>';
 										}
 
 										echo $summary;
-									echo '</div>';
-								echo '</div><!-- .bbg__announcement__flexbox -->';
+									echo '</div>'; // close ribbon text container
+								echo '</div><!-- .bbg__announcement__flexbox -->'; // close ribbon container
 							echo '</div><!-- .usa-grid -->';
-						echo '</section>';
+						echo '</section>'; // close row
 						/*** END DISPLAY OF ENTIRE RIBBON ROW ***/
 
 						elseif ( get_row_layout() == 'about_office' ):
@@ -379,6 +381,7 @@ get_header();
 							$officeEvent = false;
 							$postIDsUsed = [];
 
+							// set upcoming events query parameters
 							$qParamsUpcoming = array(
 								'post_type' => array( 'post' )
 								,'cat' => get_cat_id( 'Event' )
@@ -387,13 +390,19 @@ get_header();
 								,'order' => 'ASC'
 								,'posts_per_page' => 1
 							);
+
+							// execute upcoming events query
 							$future_events_query = new WP_Query( $qParamsUpcoming );
 							$eventDetail = [];
 
+							// if upcoming events query has posts
 							if ( $future_events_query -> have_posts() ) {
-								$officeEvent = true;
+								$officeEvent = true; // set events variable to true
+
+								// Loop through all event posts
 								while ( $future_events_query -> have_posts() ) {
 									$future_events_query -> the_post();
+									// set variables from post array
 									$id = get_the_ID();
 									$eventDetail['url'] = get_the_permalink();
 									$eventDetail['title'] = get_the_title();
@@ -403,8 +412,9 @@ get_header();
 									$postIDsUsed[] = $id;
 								}
 							}
-							$maxPosts = 4;
+							$maxPosts = 4; // set max number of events
 
+							// define office query parameters
 							$qParamsOffice = array(
 								'post_type' => array( 'post' ),
 								'posts_per_page' => $maxPosts,
@@ -413,39 +423,44 @@ get_header();
 								'order' => 'DESC',
 								'post__not_in' => $postIDsUsed
 							);
+
+							// set address variables
 							$street = get_field( 'agency_street', 'options', 'false' );
 							$city = get_field( 'agency_city', 'options', 'false' );
 							$state = get_field( 'agency_state', 'options', 'false' );
 							$zip = get_field( 'agency_zip', 'options', 'false' );
-							$address = "";
+							$address = ""; // create full address variable
 
+							// If all the address variables exists
 							if ( $street != "" && $city != "" && $state != "" && $zip != "" ) {
+								// concatenate all address variables
 								$address = $street . '<br/>' . $city . ', ' . $state . ' ' . $zip;
 
-								//Strip spaces for url-encoding.
+								// Strip spaces for url-encoding.
 								$street = str_replace( " ", "+", $street );
 								$city = str_replace( " ", "+", $city );
 								$state = str_replace( " ", "+", $state );
 								$mapLink = 'https://www.google.com/maps/place/' . $street . ',+' . $city . ',+' . $state . '+' . $zip . '/';
 
+								// set full address variable
 								$address = '<p itemprop="address" aria-label="address"><a href="'. $mapLink . '">' . $address . '</a></p>';
 							}
 							$tagLink = get_tag_link( $officeTag[0] -> term_id );
-						?>
+			// temporarily close PHP ?>
 							<style>
 								.bbg-blog__officeEvent-label { margin-top:15px !important; }
 							</style>
 
 							<article class="bbg__article bbg__kits__section">
 								<div class="usa-grid-full">
-									<?php if ($officeEvent); ?>
+									<?php if ( $officeEvent ): ; // if there are events ?>
 										<section class="usa-section">
 											<div class="usa-alert usa-alert-info">
 											    <div class="usa-alert-body">
 											      <h3 class="usa-alert-heading"><?php echo '<a href="' . $eventDetail['url'] . '">' . $eventDetail['title'] . '</a>'; ?></h3>
 											      <p class="usa-alert-text"><?php echo $eventDetail['excerpt']; ?></p>
 											    </div>
-										  </div>
+											</div>
 										</section>
 									<?php endif; ?>
 
@@ -460,7 +475,7 @@ get_header();
 														/* BEWARE: sticky posts add a record */
 														/**** START FETCH related highlights ****/
 
-														// Run query of press releases
+														// Run press releases query
 														query_posts( $qParamsOffice );
 
 														if ( have_posts() ) {
@@ -510,7 +525,6 @@ get_header();
 														echo '</ul>';
 
 														echo '<!-- Social media profiles -->';
-
 														echo '<div class="bbg__kits__social">';
 															$officeFacebook = get_sub_field( 'office_facebook' );
 															$officeTwitter = get_sub_field( 'office_twitter' );
@@ -534,8 +548,8 @@ get_header();
 									</div>
 								</div>
 							</article>
-						<?php
-						echo '</section>';
+						<?php // reopen PHP
+						echo '</section>'; // close row
 						/*** END DISPLAY OF OFFICE ROW ***/
 						endif;
 					endwhile;
