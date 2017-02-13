3d2377a4a093e006ad3aedbc02ba91539758793a
Joe Flowers <jflowers45@gmail.com>
2017-02-13 14:41:40 -0500

    Adding a placeholder for a template for playlist galleries

---
+++ page-playlist-gallery.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package bbgRedesign
+ template name: Category Browser
+
+ */
+
+$parentTitle = "";
+if($post->post_parent) {
+	$parent = $wpdb->get_row("SELECT post_title FROM $wpdb->posts WHERE ID = $post->post_parent");
+	$parentTitle = $parent->post_title;
+}
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div class="usa-grid-full">
+
+				<?php while ( have_posts() ) : the_post();
+					// IF the page is a law under the Legislation parent:
+					if ($parentTitle == "Legislation") {
+						get_template_part( 'template-parts/content-law', 'page' );
+					} else {
+						get_template_part( 'template-parts/content', 'page' );
+					}
+				?>
+
+					<div class="bbg-post-footer">
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+					</div>
+
+				<?php endwhile; // End of the loop. ?>
+			</div><!-- .usa-grid-full -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php /*get_sidebar();*/ ?>
+<?php get_footer(); ?>
