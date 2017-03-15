d3b7d6e41c707871bf4a7b909cbd85ee2d889f8f
Joe Flowers <jflowers45@gmail.com>
2017-03-14 22:25:17 -0400

    more name cleanup

--- page-customFieldChecker.php
+++ page-customFieldChecker.php
@@ -5,7 +5,7 @@
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package bbgRedesign
-   template name: Custom Field Checker
+   template name: Utility: Custom Field Checker
  */
 
 ?>
