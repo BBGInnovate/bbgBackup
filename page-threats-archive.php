b2247c706407eb90ae72c1db8526efd7a6c902aa
Gigi Frias <gigi.frias@gmail.com>
2017-04-07 13:43:32 -0400

    adding dynamic icons back into media clips

--- page-threats-archive.php
+++ page-threats-archive.php
@@ -101,7 +101,7 @@ get_header();
 
 							echo '<article class="bbg-blog__excerpt--list ">';
 								echo '<header class="entry-header bbg__article-icons-container">';
-									echo '<div class="bbg__article-icon" style="background-position: left center; background-image: url(https://www.bbg.gov/wp-content/themes/bbgRedesign/img/logo_rferl--circle-200.png);"></div>';
+									echo '<div class="bbg__article-icon" style="background-position: left center; background-image: url(' . $imgSrc . ');"></div>';
 									echo '<h3 class="entry-title" style="color:#4773aa">' . $t['headline'] . '</h3>';
 								echo '</header>';
 								echo '<div class="entry-meta bbg__excerpt-meta">';
