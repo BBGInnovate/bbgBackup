3fb268f43daf39afb84a136c2b115137f3deaf15
Gigi Frias <gigi.frias@gmail.com>
2017-04-07 13:15:18 -0400

    edits to media clips icon

--- page-threats-archive.php
+++ page-threats-archive.php
@@ -101,7 +101,7 @@ get_header();
 
 							echo '<article class="bbg-blog__excerpt--list ">';
 								echo '<header class="entry-header bbg-blog__excerpt-header">';
-									echo '<img width="25" height="25" src="https://www.bbg.gov/wp-content/themes/bbgRedesign/img/logo_rferl--circle-200.png">';
+									echo '<div style="background-size: 30px; background-position: left top; background-repeat: no-repeat; background-image: url(https://www.bbg.gov/wp-content/themes/bbgRedesign/img/logo_rferl--circle-200.png); display: table-cell;width: 35px;"></div>';
 									echo '<h3 class="entry-title" style="color:#4773aa">' . $t['headline'] . '</h3>';
 								echo '</header>';
 								echo '<div class="entry-meta bbg__excerpt-meta">';
