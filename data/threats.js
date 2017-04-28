5049a17211cdf6d2cec44bea840fbddbc9533ab2
Joe Flowers <jflowers45@gmail.com>
2017-04-28 14:24:12 -0400

    threats data

---
+++ data/threats.js
@@ -0,0 +1,291 @@
+threats=[
+    {
+        "country": "afghanistan",
+        "name": "Hatice Kamer",
+        "date": "November 28, 2016",
+        "year": "2016",
+        "niceDate": "Nov 28, 2016",
+        "status": "arrested",
+        "description": "On November 26, Hatice Kamer, a VOA Kurdish Service reporter in Diyarbakir, went to the city of Siirt to speak to the relatives of victims of a mining accident she had previously covered. Emine Erdogan, Turkish President Tayyip Erdogan\u2019s wife, was also visiting the city the same day.  Kamer and a reporter from the state-funded Anatolian News Agency were the only reporters there.  ",
+        "mugshot": "",
+        "network": [
+            "voa"
+        ],
+        "link": "http:\/\/www.voanews.com\/a\/voice-america-reporter-detained-turkey\/3612829.html",
+        "latitude": "37.927404",
+        "longitude": "41.94197800000006",
+        "headline": "VOA Kurdish reporter arrested"
+    },
+    {
+        "country": "vietnam",
+        "name": "Contractor for RFA",
+        "date": "November 28, 2016",
+        "year": "2016",
+        "niceDate": "Nov 28, 2016",
+        "status": "attacked",
+        "description": "In November, a contractor in Vietnam with Radio Free Asia\u2019s Vietnamese Service was beaten by police. Police confiscated the contractor\u2019s equipment, including his mobile phone and camera. Prior to being hired by RFA, the individual shared video with the Vietnamese Service as a citizen journalist of the mass protests in Vietnam in the wake of a chemical spill that killed tons of fish, devastating fishing communities along the country\u2019s central coast. Since the November incident, the contractor has continued to work for RFA.",
+        "mugshot": "",
+        "network": [
+            "rfa"
+        ],
+        "link": "",
+        "latitude": "14.058324",
+        "longitude": "108.277199",
+        "headline": "Beating of RFA Vietnamese Contractor"
+    },
+    {
+        "country": "cuba",
+        "name": "Independent Journalist",
+        "date": "October 16, 2016",
+        "year": "2016",
+        "niceDate": "Oct 16, 2016",
+        "status": "arrested",
+        "description": "Rolando Rodr\u00edguez Lobaina was arrested along with Manuel Le\u00f3n Vel\u00e1zquez, a journalist from Diario de Cuba. Both were released hours after receiving threats that they would go to jail.",
+        "mugshot": "",
+        "network": [
+            "ocb"
+        ],
+        "link": "http:\/\/www.martinoticias.com\/a\/detenido-director-palenque-vision-baracoa-\/131689.html",
+        "latitude": "20.3474809",
+        "longitude": "-74.50238189999999",
+        "headline": "Independent journalists harassed while trying to report chaos in Baracoa"
+    },
+    {
+        "country": "russia",
+        "name": "Two RFE\/RL journalists",
+        "date": "September 18, 2016",
+        "year": "2016",
+        "niceDate": "Sep 18, 2016",
+        "status": "detained",
+        "description": "Two journalists with RFE\/RL\u2019s Idel.Realii project were detained in a polling station in Kazan, Russia for more than an hour and a half, as they were reporting on alleged voting irregularities.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "http:\/\/www.idelreal.org\/a\/27998627.html",
+        "latitude": "55.8304",
+        "longitude": "49.0661",
+        "headline": "Journalists detained in Russia in polling station"
+    },
+    {
+        "country": "azerbaijan",
+        "name": "Two freelance journalists",
+        "date": "September 17, 2016",
+        "year": "2016",
+        "niceDate": "Sep 17, 2016",
+        "status": "detained",
+        "description": "Several journalists were detained after police intervened during a rally protesting a referendum that would give Azerbaijani president Ilham Aliyav greater powers and a longer term in office.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "http:\/\/www.rferl.org\/content\/azerbaijan-opposition-rally-baku-aliyev\/27997157.html",
+        "latitude": "40.4093",
+        "longitude": "49.8671",
+        "headline": "Two RFE\/RL freelance journalists detained during protest in Azerbaijan."
+    },
+    {
+        "country": "cambodia",
+        "name": "Chun Chanboth",
+        "date": "September 3, 2016",
+        "year": "2016",
+        "niceDate": "Sep 03, 2016",
+        "status": "threatened",
+        "description": "On September 3, 2016, a Facebook user by the name of &#8220;Oun Heang Love&#8221; posted the following message: &#8220;Kem Ley [recently assassination opposition leader] is already dead. The following people &#8211; including . . . Chun Chanboth . . . shall be dead by 2018. You can wait and see my words are true or not.&#8221; Chun Chanboth is RFA&#8217;s Cambodian Service Deputy Director.",
+        "mugshot": "",
+        "network": [
+            "rfa"
+        ],
+        "link": "",
+        "latitude": "12.5657",
+        "longitude": "104.991",
+        "headline": "Death threat against RFA journalist posted to social media"
+    },
+    {
+        "country": "armenia",
+        "name": "Three RFE\/RL journalists",
+        "date": "July 29, 2016",
+        "year": "2016",
+        "niceDate": "Jul 29, 2016",
+        "status": "threatened",
+        "description": "Three RFE\/RL Armenian Service journalists \u2014 Karlen Aslanian, Hovannes Movsisian, and Garik Harutiunian \u2014 were attacked by a large group of plainclothed men late on July 29, while covering clashes between riot police and protesters marching in support of armed gunmen who have occupied a police compound in Yerevan for the past two weeks. The attackers were armed with sticks and metal bars, and were clearly aware that they are assaulting reporters.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "https:\/\/www.bbg.gov\/blog\/2016\/08\/01\/rferl-journalists-attacked-armenia\/",
+        "latitude": "40.1792",
+        "longitude": "44.4991",
+        "headline": "Three RFE\/RL journalists attacked"
+    },
+    {
+        "country": "armenia",
+        "name": "Three journalists",
+        "date": "July 29, 2016",
+        "year": "2016",
+        "niceDate": "Jul 29, 2016",
+        "status": "attacked",
+        "description": "Three RFE\/RL Armenian Service journalists were attacked by a large group of men while covering clashes between riot police and protestors marching in support of armed gunmen who were occupying a police compound in Yerevan. The attackers were armed with sticks and metal bars, and were clearly aware that they were assaulting reporters. ",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "https:\/\/www.bbg.gov\/2016\/08\/01\/rferl-journalists-attacked-armenia\/",
+        "latitude": "40.0691",
+        "longitude": "45.0382",
+        "headline": "Three journalists attacked in Armenia"
+    },
+    {
+        "country": "cuba",
+        "name": "TV Marti Citizen-Journalists",
+        "date": "July 4, 2016",
+        "year": "2016",
+        "niceDate": "Jul 04, 2016",
+        "status": "arrested",
+        "description": "Cuban authorities have violently arrested several citizen journalists and confiscated their recording equipment along with DVDs containing Television Mart\u00ed programming. Five reporters who frequently contribute to Mart\u00ed Noticias were assaulted during a July 4th crackdown. Dozens of additional disks containing TV Mart\u00ed newscasts were confiscated in Camag\u00fcey the next day.",
+        "mugshot": "",
+        "network": [
+            "ocb"
+        ],
+        "link": "https:\/\/www.bbg.gov\/blog\/2014\/07\/11\/citizen-journalists-attacked-marti-programming-seized-in-cuba\/",
+        "latitude": "21.3926035",
+        "longitude": "-77.90531820000001",
+        "headline": "TV Mart\u00ed Citizen Journalists violently arrested"
+    },
+    {
+        "country": "vietnam",
+        "name": "Nguyen Ngoc Gia",
+        "date": "April 30, 2016",
+        "year": "2016",
+        "niceDate": "Apr 30, 2016",
+        "status": "arrested",
+        "description": "Originally arrested in 2014, the Ho Chi Minh City&#8217;s people&#8217;s court sentenced blogger Nguyen Ngoc Gia to a four-year prison term with another three years on probation for carrying out &#8220;propaganda against the state&#8221;. ",
+        "mugshot": "",
+        "network": [
+            "rfa"
+        ],
+        "link": "https:\/\/www.bbg.gov\/threats-to-press\/nguyen-ngoc-gia\/",
+        "latitude": "10.8231",
+        "longitude": "106.6297",
+        "headline": "RFA blogger sentenced to four year prison term"
+    },
+    {
+        "country": "ukraine",
+        "name": "RFE\/RL contributors in Crimea",
+        "date": "April 20, 2016",
+        "year": "2016",
+        "niceDate": "Apr 20, 2016",
+        "status": "threatened",
+        "description": "The Moscow-backed Crimean Prosecutor General\u2019s Office called for the closure of RFE\/RL\u2019s Crimea Realities website, one of the last remaining sources of independent news on Russia-annexed Crimea, claiming that the website seeks to discredit the Russian Federation, incites interethnic hatred and promotes extremism.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "http:\/\/www.bbg.gov\/blog\/2016\/04\/20\/bbg-condemns-targeting-of-independent-journalists-in-crimea-including-rferl-contributors\/",
+        "latitude": "44.95",
+        "longitude": "34.11",
+        "headline": "Crimea Realities website threatened with closure"
+    },
+    {
+        "country": "ukraine",
+        "name": "RFE\/RL contributors",
+        "date": "April 10, 2016",
+        "year": "2016",
+        "niceDate": "Apr 10, 2016",
+        "status": "threatened",
+        "description": "Police searched the home of several RFE\/RL contributors. Moscow-backed Crimean Prosecutor General&#8217;s Office says it has launched a criminal case against one of these people, Mykola Semena, alleging that he engaged in &#8220;extremism&#8221; and agitated for &#8220;violations of Russia&#8217;s territorial integrity&#8221; on the basis of his journalistic work.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "https:\/\/www.bbg.gov\/2016\/04\/20\/bbg-condemns-targeting-of-independent-journalists-in-crimea-including-rferl-contributors\/",
+        "latitude": "44.9521",
+        "longitude": "34.1024",
+        "headline": "Independent journalists targeted in Crimea, including RFE\/RL contributors"
+    },
+    {
+        "country": "united_states",
+        "name": "Reporters from RFA\u2019s Tibetan Service",
+        "date": "April 1, 2016",
+        "year": "2016",
+        "niceDate": "Apr 01, 2016",
+        "status": "threatened",
+        "description": "While covering Chinese President Xi Jinping\u2019s visit to the nuclear security summit in Washington in April, reporters from RFA\u2019s Tibetan Service were harassed by Chinese nationalist demonstrators gathering at the Convention Center.",
+        "mugshot": "",
+        "network": [
+            "rfa"
+        ],
+        "link": "https:\/\/youtu.be\/wmYLr32WSzk",
+        "latitude": "38.9036564",
+        "longitude": "-77.0229468",
+        "headline": "RFA Tibetan Reporters Harassed By Chinese Nationalists in Washington, DC"
+    },
+    {
+        "country": "cuba",
+        "name": "Independent Journalist",
+        "date": "March 24, 2016",
+        "year": "2016",
+        "niceDate": "Mar 24, 2016",
+        "status": "arrested",
+        "description": "The dissident journalist L\u00e1zaro Yuri Valle Roca was\u00a0arrested and charged with an attack last Sunday, shortly before President Obama arrived on the island.",
+        "mugshot": "",
+        "network": [
+            "ocb"
+        ],
+        "link": "http:\/\/www.martinoticias.com\/a\/periodista-independiente-acusado-de-atentado\/118127.html",
+        "latitude": "23.1135925",
+        "longitude": "-82.36659559999998",
+        "headline": "Yuri Valle Roca is released after being accused of &#8220;attack&#8221;"
+    },
+    {
+        "country": "azerbaijan",
+        "name": "Khadija Ismayilova",
+        "date": "February 5, 2016",
+        "year": "2016",
+        "niceDate": "Feb 05, 2016",
+        "status": "threatened",
+        "description": "On February 5, 2015 RFE\/RL contributor Khadija Ismayilova marked 14 months behind bars in an Azeri prison, despite widespread condemnation of her case and high-profile demands for her release.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "http:\/\/www.bbg.gov\/blog\/2016\/02\/06\/rferl-journalists-remain-behind-bars-despite-demands-for-release\/",
+        "latitude": "40.43495",
+        "longitude": "49.81",
+        "headline": "Khadija Ismayilova marks 14 months behind bars"
+    },
+    {
+        "country": "azerbaijan",
+        "name": "Khadija Ismayilova",
+        "date": "January 27, 2016",
+        "year": "2016",
+        "niceDate": "Jan 27, 2016",
+        "status": "threatened",
+        "description": "On January 27, a court in Azerbaijan prolonged Khadija Ismayilova\u2019sdetention, originally set to expire on February 5, by two months. Ismayilova was arrested on politically motivated charges on December 5 and could serve three to seven years in prison if convicted. The Broadcasting Board of Governors condemned the Azerbaijani government\u2019s imprisonment of Ismayilova and assault on freedom of expression, and called for her immediate release.",
+        "mugshot": "",
+        "network": [
+            "rferl"
+        ],
+        "link": "http:\/\/www.bbg.gov\/blog\/2015\/01\/30\/bbg-board-condemns-extended-detention-of-khadija-ismayilova\/",
+        "latitude": "40.388",
+        "longitude": "49.804",
+        "headline": "Khadija Ismayilova&#8217;s sentence extended"
+    },
+    {
+        "country": "yemen",
+        "name": "Almigdad Mojalli",
+        "date": "January 17, 2016",
+        "year": "2016",
+        "niceDate": "Jan 17, 2016",
+        "status": "killed",
+        "description": "Mojalli had been reporting for VOA on the human impact of the war and the economic crisis in Yemen. He was in rebel-held Sana?a on Sunday morning when he was killed in a Saudi-led coalition airstrike. He left behind a family of seven.",
+        "mugshot": "",
+        "network": [
+            "voa"
+        ],
+        "link": "http:\/\/www.bbg.gov\/blog\/2016\/01\/18\/bbg-voa-honor-freelance-journalist-killed-in-yemen\/",
+        "latitude": "15.369445",
+        "longitude": "44.191007",
+        "headline": "Almigdad Mojalli killed"
+    }
+];
