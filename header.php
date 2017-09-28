b76d690450da5c16e089b69430bdc79b96863dbe
Joe Flowers <jflowers45@gmail.com>
2017-09-28 15:34:41 -0400

    typo in path

--- header.php
+++ header.php
@@ -311,7 +311,7 @@ $sitewideAlert = get_field('sitewide_alert', 'option');	//off, simple, or comple
 
 				<div class="bbg-header__container">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="bbg-header__link">
-						<img src="<?php echo get_template_directory_uri() . "/$logo-agency-square"; ?>/img/logo-agency-square.png" alt="Logo for the Broadcasting Board of Governors" class="bbg-header__logo">
+						<img src="<?php echo get_template_directory_uri() . "/$squareLogo"; ?>" alt="Logo for the Broadcasting Board of Governors" class="bbg-header__logo">
 						<h1 class="bbg-header__site-title"><?php echo bbginnovate_site_name_html(); ?></h1>
 					</a>
 
