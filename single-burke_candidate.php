83f69f3c7e0a449bdc796f089f5bea59108d1842
Gigi Frias <gigi.frias@gmail.com>
2017-10-13 16:33:41 -0400

    Updates to Burke profiles

--- single-burke_candidate.php
+++ single-burke_candidate.php
@@ -8,7 +8,7 @@
 
 if ( have_posts() ) {
 	the_post();
-	$metaAuthor = get_the_author();
+	// $metaAuthor = get_the_author();
 	$metaKeywords = strip_tags( get_the_tag_list('', ', ', '') );
 	/**** CREATE OG TAGS ***/
 	$ogDescription = get_the_excerpt();
@@ -33,240 +33,166 @@ if ( have_posts() ) {
 		$bannerAdjustStr = $bannerPosition;
 	}
 
-	/**** Get profile fields *****/
-	// $occupation = get_post_meta(get_the_ID() , 'occupation', true);
-	// $email = get_post_meta(get_the_ID() , 'email', true);
-	// $phone = get_post_meta(get_the_ID() , 'phone', true);
-	// $twitterProfileHandle = get_post_meta(get_the_ID() , 'twitter_handle', true);
-	// $relatedLinksTag = get_post_meta(get_the_ID() , 'related_links_tag', true);
-	// /**** Adjustments for retired employees ***/
-	// $active = get_post_meta(get_the_ID() , 'active', true);
-	// if (!$active) {
-	// 	$occupation = "(Former) " . $occupation;
-	// }
-
-	// /*** Get the profile photo mugshot ***/
-	// $profilePhotoID = get_post_meta(get_the_ID() , 'profile_photo', true);
-	// $profilePhoto = "";
-	// if ($profilePhotoID) {
-	// 	$profilePhoto = wp_get_attachment_image_src($profilePhotoID, 'mugshot');
-	// 	$profilePhoto = $profilePhoto[0];
-	// }
-
 	rewind_posts();
 }
 
 get_header(); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-<?php
-	while ( have_posts() ):
-		the_post();
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-	// Default adds a space above header if there's no image set
-	$featuredImageClass = " bbg__article--no-featured-image";
+	<?php
+		while ( have_posts() ):
+			the_post();
 
-	$id = get_the_ID();
-	// echo "<h1>" . $id . "</h1>";
+		// Default adds a space above header if there's no image set
+		$featuredImageClass = " bbg__article--no-featured-image";
 
-	$burkeProfileObj = get_post_meta( $id, 'burke_award_info' );
+		// the title/headline field, followed by the URL and the author's twitter handle
+		$twitterText = "";
+		$twitterText.= "Profile of " . html_entity_decode( get_the_title() );
+		$twitterText.= " by @bbggov " . get_permalink();
+		$twitterURL = "//twitter.com/intent/tweet?text=" . rawurlencode( $twitterText );
+		$fbUrl = "//www.facebook.com/sharer/sharer.php?u=" . urlencode( get_permalink() );
+	?>
 
-	var_dump( $burkeProfileObj );
+		<article id="post-<?php the_ID(); ?>" <?php post_class( "bbg__article" ); ?>>
+			<div class="usa-grid">
+				<h5 class="bbg__label--mobile large">
+					<a href="<?php echo network_home_url()?>burke-awards/burke-honorees">Burke Awards honorees</a>
+				</h5>
+			</div>
 
-	// if ( $burkeProfileObj ) {
-	// 	echo get_post_field( "burke_year_of_eligibility" );
-	// }
+			<?php
+				$hideFeaturedImage = get_post_meta( get_the_ID() , "hide_featured_image", true );
+				if ( has_post_thumbnail() && ( $hideFeaturedImage != 1 ) ) {
+					echo '<div class="usa-grid-full">';
+						$featuredImageClass = "";
+						$featuredImageCutline = "";
+						$thumbnail_image = get_posts( array(
+							'p' => get_post_thumbnail_id( get_the_ID() ),
+							'post_type' => 'attachment'
+						));
+						if ( $thumbnail_image && isset( $thumbnail_image[0] ) ) {
+							$featuredImageCutline = $thumbnail_image[0] -> post_excerpt;
+						}
 
-	die();
+						$src = wp_get_attachment_image_src( get_post_thumbnail_id( $post -> ID ) , array( 700, 450 ) , false, '' );
 
-	// the title/headline field, followed by the URL and the author's twitter handle
-	/*$twitterText = "";
-	$twitterText.= "Profile of " . html_entity_decode(get_the_title());
-	$twitterText.= " by @bbggov " . get_permalink();
-	$twitterURL = "//twitter.com/intent/tweet?text=" . rawurlencode($twitterText);
-	$fbUrl = "//www.facebook.com/sharer/sharer.php?u=" . urlencode(get_permalink());*/
-?>
+						echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__article-header__banner--profile" style="background-image: url(' . $src[0] . '); background-position: ' . $bannerAdjustStr . '">';
+						echo '</div>';
+					echo '</div> <!-- usa-grid-full -->';
+				}
+			?><!-- .bbg__article-header__thumbnail -->
 
-			<article id="post-<?php
-	the_ID(); ?>" <?php
-	post_class("bbg__article"); ?>>
-				<?php
-	$hideFeaturedImage = get_post_meta(get_the_ID() , "hide_featured_image", true);
-	if (has_post_thumbnail() && ($hideFeaturedImage != 1)) {
-		echo '<div class="usa-grid-full">';
-		$featuredImageClass = "";
-		$featuredImageCutline = "";
-		$thumbnail_image = get_posts(array(
-			'p' => get_post_thumbnail_id(get_the_ID()) ,
-			'post_type' => 'attachment'
-		));
-		if ($thumbnail_image && isset($thumbnail_image[0])) {
-			$featuredImageCutline = $thumbnail_image[0]->post_excerpt;
-		}
+			<div class="usa-grid">
+			<?php
+				$burkeProfileObj = get_field( 'burke_award_info' );
+				$numRows = count ( $burkeProfileObj );
+				// create variable to sort the award array by year
+				$orderByYear = array();
+				// populate order
+				foreach( $burkeProfileObj as $i => $row ) {
+					$orderByYear[ $i ] = $row[ 'burke_ceremony_year' ];
+				}
 
-		$src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID) , array(
-			700,
-			450
-		) , false, '');
-		echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large bbg__article-header__banner--profile" style="background-image: url(' . $src[0] . '); background-position: ' . $bannerAdjustStr . '">';
-		echo '</div>';
-		echo '</div> <!-- usa-grid-full -->';
-	}
+				// use multisort to reorder the award array based on the year (reverse chrono)
+				array_multisort( $orderByYear, SORT_DESC, $burkeProfileObj );
+				// var_dump( $burkeProfileObj );
 
-?><!-- .bbg__article-header__thumbnail -->
+				echo '<header class="entry-header bbg__article-header' . $featuredImageClass . '">';
+					echo '<div class="bbg__profile-title" style="max-width: none;">';
+						the_title( '<h1 class="entry-title bbg__article-header__title">', '</h1>' );
+						echo "<!-- .bbg__article-header__title -->";
+						// output variables: ceremony year + win status
+						echo '<h5 class="entry-category bbg__profile-tagline">';
+							// check if the repeater field has rows of data
+							foreach ( array_values($burkeProfileObj) as $i => $profile ) {
+								$burkeYear = $profile[ "burke_ceremony_year" ];
+								// check if this profile won for this year
+								$burkeWin = $profile[ "burke_is_winner" ];
+								if ( !$burkeWin ) { // if not a winner, add nominee tag
+									$burkeStatus = " nominee";
+								} else { // else add winner tag
+									$burkeStatus = " winner";
+								}
 
-				<div class="bbg__article__nav">
+								echo $burkeYear . $burkeStatus;
+								if ( $numRows > 1 && $i + 1 < $numRows ) {
+									echo " | ";
+								}
+							}
+						echo "</h5><!-- .bbg__label -->";
+					echo "</div>";
+				echo "</header><!-- .bbg__article-header -->";
+			?>
+				<div class="bbg__article-sidebar--left">
+					<h3 class="bbg__sidebar-label bbg__contact-label">Share </h3>
+					<ul class="bbg__article-share">
+						<li class="bbg__article-share__link facebook">
+							<a href="<?php echo $fbUrl; ?>">
+								<span class="bbg__article-share__icon facebook"></span>
+							</a>
+						</li>
+						<li class="bbg__article-share__link twitter">
+							<a href="<?php echo $twitterURL; ?>">
+								<span class="bbg__article-share__icon twitter"></span>
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="entry-content bbg__article-content <?php echo $featuredImageClass; ?>">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+
+				<div class="bbg__article-sidebar">
 					<?php
-	echo $prevLink; ?>
-					<?php
-	echo $nextLink; ?>
-				</div><!-- .bbg__article__nav -->
+						foreach ( array_values($burkeProfileObj) as $i => $profile ) {
+							$i = $i + 1; // add a number to the item key to start at 1
+							$b = ""; // create a variable for the award content
+
+							echo "<div id='award-$i'>";
+								$burkeWin = $profile[ "burke_is_winner" ];
+									if ( !$burkeWin ) { // if not a winner, add nominee title
+										$b .= '<h3 class="bbg__label small bbg__sidebar__download__label">Nominee</h3>';
+									} else { // else add winner title
+										$b .= '<h3 class="bbg__label small bbg__sidebar__download__label">Winner</h3>';
+									}
+								$b .=  '<h5 class="bbg__sidebar-label">' . $profile[ "burke_ceremony_year" ] . '</h5>' ;
+
+								// populate network and check for service and concatenate into 1 variable
+								$burkeNetwork = $profile[ "burke_network" ];
+								$burkeService = $profile[ "burke_service" ];
+								if ( $burkeService ) {
+									$b .= " of " . $burkeNetwork . "’s" . $burkeService;
+								} else {
+									$b .= " of " . $burkeNetwork;
+								}
+
+								$burkeOccupation = $profile[ "burke_occupation" ];
 
 
-				<div class="usa-grid">
+								$burkeRecognition = $profile[ "burke_reason" ];
+								$burkeWinningWork = $profile[ "burke_sample_works" ];
+								$burkeAcceptanceURL = $profile[ "burke_acceptance_video_url" ];
 
-					<?php
-	echo '<header class="entry-header bbg__article-header' . $featuredImageClass . '">'; ?>
-						<?php
-	$noProfilePhoto = "";
-	if ($profilePhotoID) {
-?>
-							<div class="bbg__profile-photo">
-								<img src="<?php
-		echo $profilePhoto; ?>" class="bbg__profile-photo__image"/>
-							</div>
-						<?php
-	}
-	else {
-		$noProfilePhoto = "max-width: none;";
-	} ?>
-						<div class="bbg__profile-title" style="<?php
-	echo $noProfilePhoto; ?>">
+								echo $b;
+							echo "</div>";
+						}
+					?>
+class="bbg__sidebar__primary-headline"
+					<?php wp_reset_postdata();?>
+				</div><!-- .bbg__article-sidebar -->
+			</div><!-- .usa-grid -->
+		</article><!-- #post-## -->
 
-							<?php
-	the_title('<h1 class="entry-title bbg__article-header__title">', '</h1>'); ?>
+	<?php endwhile; // End of the loop. ?>
 
-							<!-- .bbg__article-header__title -->
-							<h5 class="entry-category bbg__profile-tagline">
-								<?php
-	echo $occupation; ?>
-							</h5><!-- .bbg__label -->
-
-						</div>
-					</header><!-- .bbg__article-header -->
-
-					<div class="bbg__article-sidebar--left">
-
-						<h3 class="bbg__sidebar-label bbg__contact-label">Share </h3>
-						<ul class="bbg__article-share">
-							<li class="bbg__article-share__link facebook">
-								<a href="<?php
-	echo $fbUrl; ?>">
-									<span class="bbg__article-share__icon facebook"></span>
-								</a>
-							</li>
-							<li class="bbg__article-share__link twitter">
-								<a href="<?php
-	echo $twitterURL; ?>">
-									<span class="bbg__article-share__icon twitter"></span>
-								</a>
-							</li>
-						</ul>
-
-					</div>
-
-					<div class="entry-content bbg__article-content <?php
-	echo $featuredImageClass; ?>">
-						<?php
-	the_content(); ?>
-
-						<?php
-	echo $internTagline; ?>
-
-						<!--Last modified: <?php
-	the_modified_date('F d, Y'); ?>-->
-
-					</div><!-- .entry-content -->
-
-					<div class="bbg__article-sidebar">
-
-						<?php
-	if ($email != "" || $phone != "") {
-?>
-							<h3 class="bbg__sidebar-label bbg__contact-label">Contact </h3>
-						<?php
-	}
-	elseif ($twitterProfileHandle != "") { ?>
-							<h3 class="bbg__sidebar-label bbg__contact-label">Follow on Twitter</h3>
-						<?php
-	} ?>
-
-						<ul class="bbg__article-share ">
-
-						<?php
-	if ($email != "") {
-		echo '<li class="bbg__article-share__link email"><a href="mailto:' . $email . '" title="Email ' . get_the_title() . '"><span class="bbg__article-share__icon email"></span><span class="bbg__article-share__text">' . $email . '</span></a></li>';
-	}
-
-	if ($twitterProfileHandle != "") {
-		echo '<li class="bbg__article-share__link twitter"><a href="https://twitter.com/' . $twitterProfileHandle . '" title="Follow ' . get_the_title() . ' on Twitter"><span class="bbg__article-share__icon twitter"></span><span class="bbg__article-share__text">@' . $twitterProfileHandle . '</span></a></li>';
-	}
-
-	if ($phone != "") {
-		echo '<li class="bbg__article-share__link phone"><span class="bbg__article-share__icon phone"></span><span class="bbg__article-share__text">' . $phone . '</span></li>';
-	}
-
-?>
-						</ul>
-
-
-						<?php
-	if ($relatedLinksTag != "") {
-		$qParams2 = array(
-			'post_type' => array(
-				'post'
-			) ,
-			'posts_per_page' => 5,
-			'tag' => $relatedLinksTag
-		);
-		$custom_query = new WP_Query($qParams2);
-		if ($custom_query->have_posts()) {
-			echo '<h3 class="bbg__sidebar-label">Related posts  <!--(tag "$relatedLinksTag")--></h3>';
-			echo '<ul class="bbg__profile__related-link__list">';
-			while ($custom_query->have_posts()) {
-				$custom_query->the_post();
-				$link = get_the_permalink();
-				$title = get_the_title();
-				echo '<li class="bbg__profile__related-link"><a href="' . $link . '">' . $title . '</a></li>';
-			}
-
-			echo "</ul>";
-		}
-
-		wp_reset_postdata();
-	}
-
-?>
-
-					</div><!-- .bbg__article-sidebar -->
-
-				</div><!-- .usa-grid -->
-
-			</article><!-- #post-## -->
-
-		<?php
-endwhile; // End of the loop.
- ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 	<section class="usa-grid">
-		<?php
-get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 	</section>
-<?php
-get_footer(); ?>
-
-
+<?php get_footer(); ?>
