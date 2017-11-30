8764429831d3b05ec6c5b9582cebf2308a993808
Gigi Frias <gigi.frias@gmail.com>
2017-11-30 17:47:57 -0500

    Updating metabox removal function

--- inc/bbg-functions-users.php
+++ inc/bbg-functions-users.php
@@ -17,10 +17,22 @@ function hide_from_users() {
 		$role = ( array ) $user -> roles;
 		$userRole = $role[0];
 
-		var_dump( $userRole );
+		// var_dump( $userRole );
 
-		if ( $userRole == "" ) {
-
+		if ( $userRole == "enhanced_contributor" ) {
+			function remove_my_post_metaboxes() {
+				remove_meta_box( 'authordiv','post','normal' ); // Author Metabox
+				remove_meta_box( 'formatdiv','post','normal' ); // Format Metabox
+				remove_meta_box( 'tagsdiv-post_tag','post','normal' ); // Tags Metabox
+				remove_meta_box( 'commentstatusdiv','post','normal' ); // Comments Status Metabox
+				remove_meta_box( 'commentsdiv','post','normal' ); // Comments Metabox
+				remove_meta_box( 'postcustom','post','normal' ); // Custom Fields Metabox
+				remove_meta_box( 'slugdiv','post','normal' ); // Slug Metabox
+				remove_meta_box( 'trackbacksdiv','post','normal' ); // Trackback Metabox
+				remove_meta_box( 'tagsdiv-{$organizations}','post','normal' ); // Trackback Metabox
+				// remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );  // Recent Drafts from Dashboard
+			}
+			add_action('admin_menu','remove_my_post_metaboxes');
 		}
 	} else {
 		return false;
