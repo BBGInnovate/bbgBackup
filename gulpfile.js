6d75a8010597abc1f67c4d30c27f844b2b15ab75
Kirk Radish <kirk@kirkradish.com>
2018-02-22 14:07:40 -0500

    Switch from Grunt to Gulp, Have Local Backup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+  return gulp.src('_scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(''))
+});
+
+gulp.task('autoprefixer', function(){
+    gulp.src('style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+            // cascade: false
+        }))
+        .pipe(gulp.dest(''))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('_scss/**/**/**/*.scss', ['sass', 'autoprefixer']);
+});
+
+gulp.task('default', function (callback) {
+	runSequence(['autoprefixer', 'sass', 'watch'],
+		callback
+	)
+})
